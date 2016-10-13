32d5efcb1e4e277dfe435ff7f7dbe04e08ae5773
Shlomi Gridish <sgridish@marvell.com>
2016-10-13 15:41:12 +0300

    xflags: align with autogen makefile
    Signed-off-by: Shlomi Gridish <sgridish@marvell.com>

--- build/lnx4.4_a7k_gcc/xflags.h
+++ build/lnx4.4_a7k_gcc/xflags.h
@@ -1,10 +1,14 @@
 #ifndef __XFLAGS_H__
 #define __XFLAGS_H__
 
-#define CONF_PP2_BPOOL_COOKIE_SIZE	32
+/* TODOL remove the following from here */
+#define PP2_SOC_NUM_PACKPROCS		1
 
+#define MV_DBG_LEVEL				5
+#define CONF_PP2_BPOOL_COOKIE_SIZE	64
 /** Note: in 64bits systems and user would like to use only 32bits,
  * use CONF_PP2_BPOOL_DMA_ADDR_USE_32B */
-//#define CONF_PP2_BPOOL_DMA_ADDR_USE_32B
+#define MVCONF_ARCH_DMA_ADDR_T_64BIT
+#define MVCONF_SYS_DMA_UIO
 
 #endif /* __XFLAGS_H__ */
