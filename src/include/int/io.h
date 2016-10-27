7604c32b72e7e1bcf3d22365654f9ad74dc085f4
Shlomi Gridish <sgridish@marvell.com>
2016-10-28 00:42:15 +0300

    std: io: added mem-barrieirs to writel/readl implementations
    Change-Id: I7582055703eba3a50f0c22f131887f710e4cd8c7
    Signed-off-by: Shlomi Gridish <sgridish@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/33433
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>
    Tested-by: Yuval Caduri <cyuval@marvell.com>

--- src/include/int/io.h
+++ src/include/int/io.h
@@ -33,28 +33,13 @@
 #ifndef __IO_H__
 #define __IO_H__
 
-#define __iomem
-
-/* TODO: add wmb, rmb, mb */
-
-/* TODO: improve writel/readl */
-static inline void writel(uint32_t data, uintptr_t addr)
-{
-	*(volatile uint32_t *)addr = data;
-}
-
-static inline uint32_t readl(uintptr_t addr)
-{
-	return *(volatile uint32_t *)addr;
-}
-
 #define local_irq_disable()	do{;} while(0)
 #define local_irq_enable()	do{;} while(0)
 #define local_irq_save(_flags)	do{_flags=0;} while(0)
 #define local_irq_restore(_flags)	do{_flags=_flags;} while(0)
 
+#define __iomem
 
-#if 0
 #define dsb(opt)	asm volatile("dsb " #opt : : : "memory")
 
 #define mb()		dsb(sy)
@@ -64,29 +49,65 @@ static inline uint32_t readl(uintptr_t addr)
 #define __iormb()		rmb()
 #define __iowmb()		wmb()
 
+#if 1
+/* TODO: improve writel/readl */
+//static inline void writel(u32 val, uintptr_t addr)
+static inline void writel(u32 val, volatile void __iomem *addr)
+{
+	__iowmb();
+	*(volatile u32 *)addr = val;
+}
 
+//static inline u32 readl(uintptr_t addr)
+static inline u32 readl(volatile void __iomem *addr)
+{
+	u32 val = *(volatile u32 *)addr;
+	__iormb();
+	return val;
+}
+
+
+#else
+#if 0
 /*
- * Generic IO read/write.  These perform native-endian accesses.
+ * alternative assembly primitive:
+ *
+ * If any of these .org directive fail, it means that insn1 and insn2
+ * don't have the same length. This used to be written as
+ *
+ * .if ((664b-663b) != (662b-661b))
+ *      .error "Alternatives instruction length mismatch"
+ * .endif
+ *
+ * but most assemblers die if insn1 or insn2 have a .inst. This should
+ * be fixed in a binutils release posterior to 2.25.51.0.2 (anything
+ * containing commit 4e4d08cf7399b606 or c1baaddf8861).
  */
-static inline void __raw_writeb(u8 val, volatile void __iomem *addr)
-{
-	asm volatile("strb %w0, [%1]" : : "r" (val), "r" (addr));
-}
+#define __ALTERNATIVE_CFG(oldinstr, newinstr, feature, cfg_enabled)     \
+        ".if "#cfg_enabled" == 1\n"                                     \
+        "661:\n\t"                                                      \
+        oldinstr "\n"                                                   \
+        "662:\n"                                                        \
+        ".pushsection .altinstructions,\"a\"\n"                         \
+        " .word 661b - .\n"                                             \
+        " .word 663f - .\n"                                             \
+        " .hword "#feature"\n"                                          \
+        " .byte 662b-661b\n"                                            \
+        " .byte 664f-663f\n"                                            \
+        ".popsection\n"                                                 \
+        ".pushsection .altinstr_replacement, \"a\"\n"                   \
+        "663:\n\t"                                                      \
+        newinstr "\n"                                                   \
+        "664:\n\t"                                                      \
+        ".popsection\n\t"                                               \
+        ".org   . - (664b-663b) + (662b-661b)\n\t"                      \
+        ".org   . - (662b-661b) + (664b-663b)\n"                        \
+        ".endif\n"
 
-static inline void __raw_writew(u16 val, volatile void __iomem *addr)
-{
-	asm volatile("strh %w0, [%1]" : : "r" (val), "r" (addr));
-}
+#define _ALTERNATIVE_CFG(oldinstr, newinstr, feature, cfg, ...) \
+        __ALTERNATIVE_CFG(oldinstr, newinstr, feature, IS_ENABLED(cfg))
 
-static inline void __raw_writel(u32 val, volatile void __iomem *addr)
-{
-	asm volatile("str %w0, [%1]" : : "r" (val), "r" (addr));
-}
-
-static inline void __raw_writeq(u64 val, volatile void __iomem *addr)
-{
-	asm volatile("str %0, [%1]" : : "r" (val), "r" (addr));
-}
+#define ALTERNATIVE(oldinstr, newinstr, ...)   _ALTERNATIVE_CFG(oldinstr, newinstr, __VA_ARGS__, 1)
 
 static inline u8 __raw_readb(const volatile void __iomem *addr)
 {
@@ -129,6 +150,59 @@ static inline u64 __raw_readq(const volatile void __iomem *addr)
 	return val;
 }
 
+#else
+static inline u8 __raw_readb(const volatile void __iomem *addr)
+{
+	u8 val;
+	asm volatile("ldrb %w0, [%1]" : "=r" (val) : "r" (addr));
+	return val;
+}
+
+static inline u16 __raw_readw(const volatile void __iomem *addr)
+{
+	u16 val;
+
+	asm volatile("ldrh %w0, [%1]" : "=r" (val) : "r" (addr));
+	return val;
+}
+
+static inline u32 __raw_readl(const volatile void __iomem *addr)
+{
+	u32 val;
+	asm volatile("ldr %w0, [%1]" : "=r" (val) : "r" (addr));
+	return val;
+}
+
+static inline u64 __raw_readq(const volatile void __iomem *addr)
+{
+	u64 val;
+	asm volatile("ldr %0, [%1]" : "=r" (val) : "r" (addr));
+	return val;
+}
+#endif /* 0 */
+
+/*
+ * Generic IO read/write.  These perform native-endian accesses.
+ */
+static inline void __raw_writeb(u8 val, volatile void __iomem *addr)
+{
+	asm volatile("strb %w0, [%1]" : : "r" (val), "r" (addr));
+}
+
+static inline void __raw_writew(u16 val, volatile void __iomem *addr)
+{
+	asm volatile("strh %w0, [%1]" : : "r" (val), "r" (addr));
+}
+
+static inline void __raw_writel(u32 val, volatile void __iomem *addr)
+{
+	asm volatile("str %w0, [%1]" : : "r" (val), "r" (addr));
+}
+
+static inline void __raw_writeq(u64 val, volatile void __iomem *addr)
+{
+	asm volatile("str %0, [%1]" : : "r" (val), "r" (addr));
+}
 
 
 /*
@@ -160,7 +234,6 @@ static inline u64 __raw_readq(const volatile void __iomem *addr)
 #define writew(v,c)		({ __iowmb(); writew_relaxed((v),(c)); })
 #define writel(v,c)		({ __iowmb(); writel_relaxed((v),(c)); })
 #define writeq(v,c)		({ __iowmb(); writeq_relaxed((v),(c)); })
-
-#endif
+#endif /* 0 */
 
 #endif /* __IO_H__ */
