c679d23e6a4cc93072308113dafe7862936acb4c
Alan Winkowski <walan@marvell.com>
2016-11-14 20:36:32 +0200

    pp2: update mv_pp2_cls.h and mv_net.h
    1) update mv_pp2_cls.h and mv_net.h to reflect latest changes
       - changes according to naming conventions
       - modified pp2_cls_tbl_rule to be filled per field instead of
         filling one string
       - commented code that will be used only in future phases
    2) pass checkpatch on all the file. One warning on copyright header
       (will be changed in another patch, since needs to be fixed for all
        files)
    
    Change-Id: Ic60bf3dfaec62e436cf52ea5d043f94f82deca2b
    Signed-off-by: Alan Winkowski <walan@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/33771

--- src/include/drivers/mv_pp2_cls.h
+++ src/include/drivers/mv_pp2_cls.h
@@ -6,15 +6,15 @@
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are met:
 
-  	* Redistributions of source code must retain the above copyright
+	* Redistributions of source code must retain the above copyright
 	  notice, this list of conditions and the following disclaimer.
 
-  	* Redistributions in binary form must reproduce the above copyright
-  	  notice, this list of conditions and the following disclaimer in the
-  	  documentation and/or other materials provided with the distribution.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
 
-  	* Neither the name of Marvell nor the names of its contributors may be
-  	  used to endorse or promote products derived from this software
+	* Neither the name of Marvell nor the names of its contributors may be
+	  used to endorse or promote products derived from this software
 	  without specific prior written permission.
 
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -36,19 +36,17 @@
 #include "mv_std.h"
 #include "mv_net.h"
 
-
 struct pp2_cls_tbl;
-
 
 #define PP2_CLS_TBL_MAX_NUM_FIELDS	5
 
-
 enum pp2_cls_tbl_type {
-	PP2_CLS_TBL_T_EM = 0,
-	PP2_CLS_TBL_T_EM_w_MASK
+	PP2_CLS_TBL_EXACT_MATCH = 0,
+	PP2_CLS_TBL_MASKABLE
 };
 
-/* TODO: Future
+/**
+ * TODO: Future
  * enum pp2_cls_tbl_statistics_mode {
  *	PP2_CLS_TBL_STATS_M_NONE = 0,
  *	PP2_CLS_TBL_STATS_M_FRM,
@@ -56,80 +54,126 @@ enum pp2_cls_tbl_type {
  * };
  */
 
-enum pp2_cls_tbl_entry_action_type {
-	PP2_CLS_TBL_ACT_T_DROP = 0,
-	PP2_CLS_TBL_ACT_T_LU,
-	PP2_CLS_TBL_ACT_T_DONE,
+enum pp2_cls_tbl_action_type {
+	PP2_CLS_TBL_ACT_DROP = 0,
+	PP2_CLS_TBL_ACT_DONE,
+	/* TODO: PP2_CLS_TBL_ACT_LU */
 };
 
-struct cls_cos_desc {
+struct pp2_cls_cos_desc {
 	struct pp2_ppio	*ppio;
 	u8		 tc;
 };
 
-struct pp2_cls_tbl_entry_action {
-	enum pp2_cls_tbl_entry_action_type		type;
-	struct pp2_cls_tbl		*next_tbl;
+struct pp2_cls_tbl_action {
+	enum pp2_cls_tbl_action_type		type;
+	/* TODO: struct pp2_cls_tbl			*next_tbl; */
 	/**< valid only in case of next-action is LU */
-
-#if 0 /* future ... */
-	enum pp2_cls_tbl_mark_type	 mark_type;
-	union {
-		u16			 flow_id;
-		u16			 qos;
-	} u;
-#endif /* 0 */
-
-	/* 'NULL' value means no-cos change; i.e. keep original cos */
-	struct cls_cos_desc		*cos;
+	/* TODO: enum pp2_cls_tbl_mark_type	 mark_type; */
+	/*
+	* TODO :
+	* union {
+	*	u16			 flow_id;
+	*	u16			 qos;
+	* } u;
+	*/
+	/** 'NULL' value means no-cos change; i.e. keep original cos */
+	struct pp2_cls_cos_desc		*cos;
 };
 
 struct pp2_cls_tbl_key {
-	u8				 key_size;
-	u8				 num_fields;
+	u8				key_size;
+	u8				num_fields;
 	struct {
-		enum mv_net_prtcl	 prtcl;
-		mv_net_prtcl_field_t	 field;
-	} prtcl_fld[PP2_CLS_TBL_MAX_NUM_FIELDS];
+		enum mv_net_proto		proto;
+		union mv_net_proto_fields	field;
+	} proto_field[PP2_CLS_TBL_MAX_NUM_FIELDS];
 };
 
 struct pp2_cls_tbl_params {
 	enum pp2_cls_tbl_type			 type;
 	u16					 max_num_rules;
 	struct pp2_cls_tbl_key			 key;
-
 	/* TODO: enum pp2_cls_tbl_statistics_mode	 stats_mode; */
-
 	/* TODO: enum pp2_cls_tbl_aging_mode	 aging_mode; */
 	/* TODO: enum pp2_cls_tbl_priority_mode	 prio_mode; */
-
-	struct pp2_cls_tbl_entry_action		 dflt_act;
+	struct pp2_cls_tbl_action		 default_act;
 };
 
+/**
+ * Add a classifier rule
+ *
+ * @param[in]	*params		A pointer to the classifier table parameters
+ *
+ * @param[out]	**tbl		A pointer to an allocated classifier table
+ *
+ * @retval		0 on success
+ * @retval		error-code otherwise
+ */
 int pp2_cls_tbl_init(struct pp2_cls_tbl_params *params, struct pp2_cls_tbl **tbl);
+
+/**
+ * Deinit a classifier table object
+ *
+ * @param[in]	*tbl		A pointer to a classifier table object
+ *
+ */
 void pp2_cls_tbl_deinit(struct pp2_cls_tbl *tbl);
 
-struct pp2_cls_tbl_rule {
-	union {
-		struct {
-			u8	size;
-			void	*key;
-		} em;
-		struct {
-			u8	size;
-			void	*key;
-			void	*mask;
-		} em_w_mask;
-	} key;
+struct pp2_cls_rule_key_field {
+	u8	size;
+	u8	*key;
+	u8	*mask;
 };
 
-int pp2_cls_tbl_add_rule(struct pp2_cls_tbl			*tbl,
-			 struct pp2_cls_tbl_rule		*rule,
-			 struct pp2_cls_tbl_entry_action	*action);
-int pp2_cls_tbl_modify_rule(struct pp2_cls_tbl			*tbl,
-			    struct pp2_cls_tbl_rule		*rule,
-			    struct pp2_cls_tbl_entry_action	*action);
-int pp2_cls_tbl_remove_rule(struct pp2_cls_tbl			*tbl,
-			    struct pp2_cls_tbl_rule		*rule);
+/*TODO : Add union to support future API's */
+
+struct pp2_cls_tbl_rule {
+	u8	num_fields;
+	struct pp2_cls_rule_key_field fields[PP2_CLS_TBL_MAX_NUM_FIELDS];
+};
+
+/**
+ * Add a classifier rule
+ *
+ * @param[in]	tbl		A pointer to a classifier table object
+ * @param[in]	rule		A pointer to a classifier rule
+ * @param[in]	action		A pointer to a classifier action
+ *
+ * @retval	0 on success
+ * @retval	positive value - reserved
+ * @retval	error-code otherwise (negative value)
+ */
+int pp2_cls_tbl_add_rule(struct pp2_cls_tbl		*tbl,
+			 struct pp2_cls_tbl_rule	*rule,
+			 struct pp2_cls_tbl_action	*action);
+
+/**
+ * Modify the action of an existing classifier rule
+ *
+ * The rule must be already in place
+ *
+ * @param[in]	tbl		A pointer to a classifier table object
+ * @param[in]	rule		A pointer to a classifier rule
+ * @param[in]	action		A pointer to a classifier action
+ *
+ * @retval	0 on success
+ * @retval	error-code otherwise
+ */
+int pp2_cls_tbl_modify_rule(struct pp2_cls_tbl		*tbl,
+			    struct pp2_cls_tbl_rule	*rule,
+			    struct pp2_cls_tbl_action	*action);
+
+/**
+ * Remove a classifier rule
+ *
+ * @param[in]	tbl		A pointer to a classifier table object
+ * @param[in]	rule		A pointer to a classifier rule
+ *
+ * @retval		0 on success
+ * @retval		error-code otherwise
+ */
+int pp2_cls_tbl_remove_rule(struct pp2_cls_tbl		*tbl,
+			    struct pp2_cls_tbl_rule	*rule);
 
 #endif /* __MV_PP2_CLS_H__ */
--- src/include/mv_net.h
+++ src/include/mv_net.h
@@ -6,15 +6,15 @@
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are met:
 
-  	* Redistributions of source code must retain the above copyright
+	* Redistributions of source code must retain the above copyright
 	  notice, this list of conditions and the following disclaimer.
 
-  	* Redistributions in binary form must reproduce the above copyright
-  	  notice, this list of conditions and the following disclaimer in the
-  	  documentation and/or other materials provided with the distribution.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
 
-  	* Neither the name of Marvell nor the names of its contributors may be
-  	  used to endorse or promote products derived from this software
+	* Neither the name of Marvell nor the names of its contributors may be
+	  used to endorse or promote products derived from this software
 	  without specific prior written permission.
 
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -33,22 +33,6 @@
 #ifndef __MV_NET_H__
 #define __MV_NET_H__
 
-#define MV_NET_MAX_NUM_FIELDS	32
-
-#define MV_NET_PROT_ETH_SHIFT	1
-#define MV_NET_PROT_VLAN_SHIFT	2
-#define MV_NET_PROT_PPPoE_SHIFT	3
-#define MV_NET_PROT_IPv4_SHIFT	4
-#define MV_NET_PROT_IPv6_SHIFT	5
-#define MV_NET_PROT_IP_SHIFT	6
-#define MV_NET_PROT_L4_SHIFT	7
-#define MV_NET_PROT_UDP_SHIFT	8
-#define MV_NET_PROT_TCP_SHIFT	9
-#define MV_NET_PROT_ICMP_SHIFT	10
-#define MV_NET_PROT_ARP_SHIFT	11
-#define MV_NET_PROT_LAST_SHIFT	20
-
-
 enum mv_net_eth_fields {
 	MV_NET_ETH_F_SA = 0,
 	MV_NET_ETH_F_DA,
@@ -62,18 +46,18 @@ enum mv_net_vlan_fields {
 };
 
 enum mv_net_ipv4_fields {
-	MV_NET_IPv4_F_TOS = 0,
-	MV_NET_IPv4_F_SA,
-	MV_NET_IPv4_F_DA,
-	MV_NET_IPv4_F_PROTO
+	MV_NET_IP4_F_TOS = 0,
+	MV_NET_IP4_F_SA,
+	MV_NET_IP4_F_DA,
+	MV_NET_IP4_F_PROTO
 };
 
 enum mv_net_ipv6_fields {
-	MV_NET_IPv6_F_TC = 0,
-	MV_NET_IPv6_F_SA,
-	MV_NET_IPv6_F_DA,
-	MV_NET_IPv6_F_FLOW,
-	MV_NET_IPv6_F_NEXT_HDR
+	MV_NET_IP6_F_TC = 0,
+	MV_NET_IP6_F_SA,
+	MV_NET_IP6_F_DA,
+	MV_NET_IP6_F_FLOW,
+	MV_NET_IP6_F_NEXT_HDR
 };
 
 enum mv_net_l4_fields {
@@ -94,43 +78,30 @@ enum mv_net_tcp_fields {
 	MV_NET_TCP_F_CSUM
 };
 
-/*
-enum mv_net_prtcl_fields {
-	union {
-		enum mv_net_eth_fields	eth;
-		enum mv_net_vlan_fields	vlan;
-		enum mv_net_ipv4_fields	ipv4;
-		enum mv_net_ipv6_fields	ipv6;
-		enum mv_net_l4_fields	l4;
-		enum mv_net_udp_fields	udp;
-		enum mv_net_tcp_fields	tcp;
-	} u;
-};
-*/
-
-enum mv_net_prtcl {
-	MV_NET_PRTCL_NONE	= 0,
-
-	MV_NET_PRTCL_ETH	= (MV_NET_PROT_ETH_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_VLAN	= (MV_NET_PROT_VLAN_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_PPPoE	= (MV_NET_PROT_PPPoE_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_IP		= (MV_NET_PROT_IP_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_IPv4	= (MV_NET_PROT_IPv4_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_IPv6	= (MV_NET_PROT_IPv6_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_L4		= (MV_NET_PROT_L4_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_TCP	= (MV_NET_PROT_TCP_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_UDP	= (MV_NET_PROT_UDP_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_ICMP	= (MV_NET_PROT_ICMP_SHIFT*MV_NET_MAX_NUM_FIELDS),
-	MV_NET_PRTCL_ARP	= (MV_NET_PROT_ARP_SHIFT*MV_NET_MAX_NUM_FIELDS),
-
-	MV_NET_PRTCL_LAST	= (MV_NET_PROT_LAST_SHIFT*MV_NET_MAX_NUM_FIELDS)
+enum mv_net_proto {
+	MV_NET_PROTO_NONE	= 0,
+	MV_NET_PROTO_ETH,
+	MV_NET_PROTO_VLAN,
+	MV_NET_PROTO_PPPOE,
+	MV_NET_PROTO_IP,
+	MV_NET_PROTO_IP4,
+	MV_NET_PROTO_IP6,
+	MV_NET_PROTO_L4,
+	MV_NET_PROTO_TCP,
+	MV_NET_PROTO_UDP,
+	MV_NET_PROTO_ICMP,
+	MV_NET_PROTO_ARP,
+	MV_NET_PROTO_LAST
 };
 
-typedef u16 mv_net_prtcl_field_t;
-
-/*
-#define MV_NET_PRTCL_FIELD(_hdr, _fld)	\
-	(mv_net_prtcl_field_t)((_hdr*))
-*/
+union mv_net_proto_fields {
+	enum mv_net_eth_fields	eth;
+	enum mv_net_vlan_fields	vlan;
+	enum mv_net_ipv4_fields	ipv4;
+	enum mv_net_ipv6_fields	ipv6;
+	enum mv_net_l4_fields	l4;
+	enum mv_net_udp_fields	udp;
+	enum mv_net_tcp_fields	tcp;
+};
 
 #endif /* __MV_NET_H__ */
