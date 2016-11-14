d1366606a17ac220e06e81cd9537ad9cd48680d1
Shlomi Gridish <sgridish@marvell.com>
2016-11-14 10:19:34 +0200

    apps: pkt-echo: added command-line options parsing
    Change-Id: I8b431477f40cdad3f2abb8de8a99a441e8bdf326
    Signed-off-by: Shlomi Gridish <sgridish@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/33733
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>
    Tested-by: Yuval Caduri <cyuval@marvell.com>

--- apps/examples/ppv2/pkt-echo/pkt_echo.c
+++ apps/examples/ppv2/pkt-echo/pkt_echo.c
@@ -45,19 +45,36 @@
 
 
 #define BUF_LEN		2048
+#define NUM_BUFFS	8192
 #define Q_SIZE		1024
-#define BURST_SIZE	16
+#define MAX_BURST_SIZE	(Q_SIZE)>>1
+#define DFLT_BURST_SIZE	256
 #define PKT_OFFS	64
 #define PP2_MH_SIZE	(2) /* TODO: take this from ppio definitions.*/
 #define PKT_EFEC_OFFS	(PKT_OFFS+PP2_MH_SIZE)
+#define MAX_PPIOS	1
+#define MAX_NUM_CORES	1
 
-#define DO_PKT_ECHO
+#define PKT_ECHO_SUPPORT
 #define USE_APP_PREFETCH
 #define PREFETCH_SHIFT	4
+
+/** Get rid of path in filename - only for unix-type paths using '/' */
+#define NO_PATH(file_name) (strrchr((file_name), '/') ? \
+			    strrchr((file_name), '/') + 1 : (file_name))
 
 
 struct glob_arg {
 	int			 verbose;
+	int			 cli;
+	int			 cpus;	/* cpus used for running */
+	u16			 burst;
+	int			 affinity;
+	int			 loopback;
+	int			 echo;
+	u64			 qs_map;
+	int			 qs_map_shift;
+
 	struct pp2_hif		*hif;
 	struct pp2_bpool	*pool;
 	struct pp2_ppio		*port;
@@ -72,6 +89,7 @@ struct local_arg {
 static struct glob_arg garg = {};
 
 
+#ifdef PKT_ECHO_SUPPORT
 #ifdef USE_APP_PREFETCH
 static inline void prefetch(const void *ptr)
 {
@@ -79,7 +97,6 @@ static inline void prefetch(const void *ptr)
 }
 #endif /* USE_APP_PREFETCH */
 
-#ifdef DO_PKT_ECHO
 static inline void swap_l2(char *buf)
 {
 	uint16_t *eth_hdr;
@@ -106,13 +123,13 @@ static inline void swap_l3(char *buf)
 	((uint32_t *)buf)[0] = ((uint32_t *)buf)[1];
 	((uint32_t *)buf)[1] = tmp32;
 }
-#endif /* DO_PKT_ECHO */
+#endif /* PKT_ECHO_SUPPORT */
 
 
 static int main_loop(void *arg, volatile int *running)
 {
 	struct glob_arg		*garg = (struct glob_arg *)arg;
-	struct pp2_ppio_desc	 descs[BURST_SIZE];
+	struct pp2_ppio_desc	 descs[MAX_BURST_SIZE];
 	int			 err;
 	u16			 i,num;
 
@@ -122,7 +139,7 @@ static int main_loop(void *arg, volatile int *running)
 	}
 
 	while (*running) {
-		num = BURST_SIZE;
+		num = garg->burst;
 		err = pp2_ppio_recv(garg->port, 0, 0, descs, &num);
 
 		for (i=0; i<num; i++) {
@@ -131,19 +148,21 @@ static int main_loop(void *arg, volatile int *running)
 			u16 len = pp2_ppio_inq_desc_get_pkt_len(&descs[i]) - PP2_MH_SIZE;
 
 //printf("packet:\n"); mem_disp(buff, len);
-#ifdef DO_PKT_ECHO
+#ifdef PKT_ECHO_SUPPORT
+			if (garg->echo) {
 #ifdef USE_APP_PREFETCH
-			if (num-i > PREFETCH_SHIFT) {
-				char *tmp_buff = (char *)(uintptr_t)pp2_ppio_inq_desc_get_cookie(&descs[i+PREFETCH_SHIFT]);
-				tmp_buff += PKT_EFEC_OFFS;
-				prefetch(tmp_buff);
-			}
+				if (num-i > PREFETCH_SHIFT) {
+					char *tmp_buff = (char *)(uintptr_t)pp2_ppio_inq_desc_get_cookie(&descs[i+PREFETCH_SHIFT]);
+					tmp_buff += PKT_EFEC_OFFS;
+					prefetch(tmp_buff);
+				}
 #endif /* USE_APP_PREFETCH */
 
-			swap_l2(buff);
-			swap_l3(buff);
+				swap_l2(buff);
+				swap_l3(buff);
 //printf("packet:\n"); mem_disp(buff, len);
-#endif /* DO_PKT_ECHO */
+			}
+#endif /* PKT_ECHO_SUPPORT */
 
 			pp2_ppio_outq_desc_reset(&descs[i]);
 			pp2_ppio_outq_desc_set_phys_addr(&descs[i], pa);
@@ -151,18 +170,6 @@ static int main_loop(void *arg, volatile int *running)
 			pp2_ppio_outq_desc_set_pkt_offset(&descs[i], PKT_EFEC_OFFS);
 			pp2_ppio_outq_desc_set_pkt_len(&descs[i], len);
 			pp2_ppio_outq_desc_set_pool(&descs[i], garg->pool);
-/*
-printf("tx-desc:\n");
-printf("desc@0:\n\t%08X %08X %08X %08X\n\t\%08X %08X %08X %08X\n",
-descs[i].cmds[0],
-descs[i].cmds[1],
-descs[i].cmds[2],
-descs[i].cmds[3],
-descs[i].cmds[4],
-descs[i].cmds[5],
-descs[i].cmds[6],
-descs[i].cmds[7]);
-*/
 		}
 
 		if (num && ((err = pp2_ppio_send(garg->port, garg->hif, 0, descs, &num)) != 0))
@@ -221,7 +228,7 @@ static int init_local_modules(struct glob_arg *garg)
 
 	memset(&bpool_params, 0, sizeof(bpool_params));
 	bpool_params.match = "pool-0:0";
-	bpool_params.max_num_buffs = 8192;
+	bpool_params.max_num_buffs = NUM_BUFFS;
 	bpool_params.buff_len = BUF_LEN;
 	if ((err = pp2_bpool_init(&bpool_params, &garg->pool)) != 0)
 		return err;
@@ -337,18 +344,149 @@ static void deinit_local(void *arg)
 		return;
 }
 
+static void usage(char *progname)
+{
+	printf("\n"
+	       "MUSDK packet-echo application.\n"
+	       "\n"
+	       "Usage: %s OPTIONS\n"
+	       "  E.g. %s -i eth0,eth1 -c 1\n"
+	       "\n"
+	       "Mandatory OPTIONS:\n"
+	       "\t-i, --interface <Eth-interfaces> (comma-separated, no spaces)\n"
+	       "                  Interface count min 1, max %i\n"
+	       "\n"
+	       "Optional OPTIONS:\n"
+	       "\t-b <size>                Burst size (default is %d)\n"
+	       "\t-c, --cores <number>     Number of CPUs to use.\n"
+	       "\t-a, --affinity <number>  Use setaffinity (default is no affinity).\n"
+	       "\t--no-echo                No Echo packets\n"
+	       "\t--cli                    Use CLI\n"
+	       "\t?, -h, --help            Display help and exit.\n\n"
+	       "\n", NO_PATH(progname), NO_PATH(progname), MAX_PPIOS, MAX_BURST_SIZE
+	       );
+}
+
+static int parse_args(struct glob_arg *garg, int argc, char *argv[])
+{
+	int	i = 1;
+
+	garg->cli = 0;
+	garg->cpus = 1;
+	garg->affinity = -1;
+	garg->burst = DFLT_BURST_SIZE;
+	garg->echo = 1;
+	garg->qs_map = 0;
+	garg->qs_map_shift = 0;
+
+	while (i < argc) {
+		if ((strcmp(argv[i], "?") == 0) ||
+		    (strcmp(argv[i], "-h") == 0) ||
+		    (strcmp(argv[i], "--help") == 0)) {
+			usage(argv[0]);
+			exit(0);
+		} else if (strcmp(argv[i], "-i") == 0) {
+			if (argc < (i+2)) {
+				pr_err("Invalid number of arguments!\n");
+				return -EINVAL;
+			}
+			if (argv[i+1][0] == '-') {
+				pr_err("Invalid arguments format!\n");
+				return -EINVAL;
+			}
+			/* TODO: complete .... */
+			i += 2;
+		} else if (strcmp(argv[i], "-b") == 0) {
+			if (argc < (i+2)) {
+				pr_err("Invalid number of arguments!\n");
+				return -EINVAL;
+			}
+			if (argv[i+1][0] == '-') {
+				pr_err("Invalid arguments format!\n");
+				return -EINVAL;
+			}
+			garg->burst = atoi(argv[i+1]);
+			i += 2;
+		} else if (strcmp(argv[i], "-c") == 0) {
+			if (argc < (i+2)) {
+				pr_err("Invalid number of arguments!\n");
+				return -EINVAL;
+			}
+			if (argv[i+1][0] == '-') {
+				pr_err("Invalid arguments format!\n");
+				return -EINVAL;
+			}
+			garg->cpus = atoi(argv[i+1]);
+			i += 2;
+		} else if (strcmp(argv[i], "-a") == 0) {
+			garg->affinity = atoi(argv[i+1]);
+			i += 2;
+		} else if (strcmp(argv[i], "-m") == 0) {
+			char *token;
+			if (argc < (i+2)) {
+				pr_err("Invalid number of arguments!\n");
+				return -EINVAL;
+			}
+			if (argv[i+1][0] == '-') {
+				pr_err("Invalid arguments format!\n");
+				return -EINVAL;
+			}
+			token = strtok(argv[i+1], ":");
+			sscanf(token,"%x", (unsigned int *)&garg->qs_map);
+			token = strtok(NULL, "");
+			garg->qs_map_shift = atoi(token);
+			i += 2;
+		} else if (strcmp(argv[i], "--no-echo") == 0) {
+			garg->echo = 0;
+			i += 1;
+		} else if (strcmp(argv[i], "--cli") == 0) {
+			garg->cli = 1;
+			i += 1;
+		} else {
+			pr_err("argument (%s) not supported!\n", argv[i]);
+			return -EINVAL;
+		}
+	}
+
+	/* Now, check validity of all inputs */
+	if (garg->burst > MAX_BURST_SIZE) {
+		pr_err("illegal burst size requested (%d vs %d)!\n",
+			garg->burst, MAX_BURST_SIZE);
+		return -EINVAL;
+	}
+	if (garg->cpus > MAX_NUM_CORES) {
+		pr_err("illegal burst size requested (%d vs %d)!\n",
+			garg->cpus, MAX_NUM_CORES);
+		return -EINVAL;
+	}
+	if (garg->qs_map || garg->qs_map_shift) {
+		pr_err("Queues mapping not supported yet!\n");
+		return -ENOTSUP;
+	}
+	if (garg->affinity != -1) {
+		pr_err("Affinity not supported yet!\n");
+		return -ENOTSUP;
+	}
+
+	return 0;
+}
+
 
 int main (int argc, char *argv[])
 {
 	struct mvapp_params	mvapp_params;
+	int			err;
 
 	setbuf(stdout, NULL);
+
+	if ((err = parse_args(&garg, argc, argv)) != 0)
+		return err;
 
 	printf("Marvell Armada US (Build: %s %s)\n", __DATE__, __TIME__);
 
 	memset(&mvapp_params, 0, sizeof(mvapp_params));
-	mvapp_params.use_cli		= 1;
-	mvapp_params.num_cores		= 1;
+	mvapp_params.use_cli		= garg.cli;
+	mvapp_params.num_cores		= garg.cpus;
 	mvapp_params.global_arg		= (void *)&garg;
 	mvapp_params.init_global_cb	= init_global;
 	mvapp_params.deinit_global_cb	= deinit_global;
