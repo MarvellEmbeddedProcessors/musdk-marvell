8e563fc50675efb6c2bf8d8c891b1654b68932d1
Yuval Caduri <cyuval@marvell.com>
2016-11-15 11:50:31 +0200

    pp2: flags: Update building flags to support 32-bit buffer address.
    Make pp2 bpool dma_address = 32 bit.
    Make pp2 cookie (virt_addr) = 32 bit.
    Note: pkt_echo app is broken.
    
    Change-Id: I61f5f7ae6828bde32efc0b443618eb9fe3e8100a
    Signed-off-by: Yuval Caduri <cyuval@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/33794

--- build/lnx4.4_a7k_gcc/xflags.h
+++ build/lnx4.4_a7k_gcc/xflags.h
@@ -6,10 +6,12 @@
 
 #define MVCONF_DBG_LEVEL			5
 
-#define CONF_PP2_BPOOL_COOKIE_SIZE	64
+#define CONF_PP2_BPOOL_COOKIE_SIZE	32
 /** Note: in 64bits systems and user would like to use only 32bits,
  * use CONF_PP2_BPOOL_DMA_ADDR_USE_32B */
 #define MVCONF_ARCH_DMA_ADDR_T_64BIT
+#define CONF_PP2_BPOOL_DMA_ADDR_USE_32B
+
 #define MVCONF_SYS_DMA_UIO
 
 #endif /* __XFLAGS_H__ */
