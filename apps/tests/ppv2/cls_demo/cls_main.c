87f34cc9ddeb73d0414393f7ba0de8c993fc53c5
Liron Himi <lironh@marvell.com>
2017-08-28 11:19:17 +0300

    apps: fixed cls-demo main usage
    Change-Id: I4b208ea93bf76a12a8b0c96c2fffe3a9d1aafc20
    Signed-off-by: Liron Himi <lironh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/43439
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>

--- apps/tests/ppv2/cls_demo/cls_main.c
+++ apps/tests/ppv2/cls_demo/cls_main.c
@@ -466,7 +466,7 @@ static void usage(char *progname)
 		"\t-c, --cores <number>		Number of CPUs to use\n"
 		"\t-t, --num_tcs <number>	Number of Traffic classes (TCs) to use\n"
 		"\t-b, --hash_type <none, 2-tuple, 5-tuple>\n"
-		"\t--ppio_tag_mode		(no argument)configure ppio_tag_mode parameter\n"
+		"\t--eth_start_hdr		(no argument)configure ethernet start header\n"
 		"\t--logical_port_params	(no argument)configure logical port parameters\n"
 		"\t--policers_range		(dec)-(dec) valid range [1-%d]\n"
 		"\t--policer_params		(no argument)configure default policer parameters\n"
