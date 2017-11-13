9f4db28b3bfb33d0f6912d973e4176f1b47ae582
Alan Winkowski <walan@marvell.com>
2017-11-13 18:32:57 +0200

    apps: fix: move cli descriptors cmd
    - move cli descriptos command to a different function
      to allow calling this command independently from
      different applications.
    
    Change-Id: I04ed8b3385ce8746872266569df166d39f030cd3
    Signed-off-by: Alan Winkowski <walan@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/46276
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>

--- apps/tests/ppv2/pp2_tests_main.c
+++ apps/tests/ppv2/pp2_tests_main.c
@@ -300,6 +300,7 @@ static int register_cli_cmds(struct glob_arg *garg)
 	register_cli_c2_cmds(ppio);
 	register_cli_qos_cmds(ppio);
 	register_cli_prs_cmds(ppio);
+	app_register_cli_desc_cmds(pp2_args->ports_desc);
 	app_register_cli_common_cmds(pp2_args->ports_desc);
 
 	return 0;
