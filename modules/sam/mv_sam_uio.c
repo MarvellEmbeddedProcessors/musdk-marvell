c0c1987740bf34be69128e5275b097cafe067343
Dmitri Epshtein <dima@marvell.com>
2017-11-22 16:32:46 +0200

    sam_uio: Register only free rings as UIO devices
    enable eip197 clock if not enabled by crypto_safexcel.ko yet
    
    Change-Id: Ifc27d5178df629c9a70ff21a989a26eeba7fe693
    Signed-off-by: Dmitri Epshtein <dima@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/46697
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>

--- modules/sam/mv_sam_uio.c
+++ modules/sam/mv_sam_uio.c
@@ -41,6 +41,8 @@
 
 #define SAM_RING_OFFS(ring)		((ring) * 0x1000)
 
+#define HIA_CDR_BASE_ADDR_LO_REG	0x0
+
 #define HIA_RDR_REGS_OFFSET		0x800
 #define HIA_RDR_STAT_REG		(HIA_RDR_REGS_OFFSET + 0x3C)
 
@@ -76,9 +78,11 @@ struct sam_uio_ring_info {
 struct sam_uio_pdev_info {
 	int uio_num;
 	struct device *dev;
+	struct clk *clk;
 	char name[16];
 	char *regs_vbase;
-	int rings_num;
+	u32 hw_rings_num;
+	u32 rings_map;
 	struct sam_uio_ring_info *rings;
 };
 
@@ -92,9 +96,29 @@ static inline int sam_uio_get_rings_num(struct sam_uio_pdev_info *pdev_info)
 	addr = (u32 *)(pdev_info->regs_vbase + SAM_HIA_OPTIONS_REG);
 
 	val32 = readl(addr);
-	pdev_info->rings_num = val32 & SAM_N_RINGS_MASK;
+	pdev_info->hw_rings_num = val32 & SAM_N_RINGS_MASK;
 
-	return pdev_info->rings_num;
+	return pdev_info->hw_rings_num;
+}
+
+static inline int sam_uio_ring_is_busy(struct sam_uio_ring_info *ring_priv)
+{
+	u32 *addr, val32;
+
+	addr = (u32 *)(ring_priv->xdr_regs_vbase + HIA_CDR_BASE_ADDR_LO_REG);
+
+	val32 = readl(addr);
+
+	return (val32 != 0);
+}
+
+static inline void sam_uio_reset_ring(struct sam_uio_ring_info *ring_priv)
+{
+	u32 *addr;
+
+	addr = (u32 *)(ring_priv->xdr_regs_vbase + HIA_CDR_BASE_ADDR_LO_REG);
+
+	writel(0, addr);
 }
 
 static inline void sam_uio_ring_ack_irq(struct sam_uio_ring_info *ring_priv, u32 mask)
@@ -143,12 +167,11 @@ static int sam_uio_probe(struct platform_device *pdev)
 		dev_err(dev, "Non DT case is not supported\n");
 		return -EINVAL;
 	}
-
 	pdev_info = devm_kzalloc(dev, sizeof(struct sam_uio_pdev_info),
 				   GFP_KERNEL);
 	if (!pdev_info) {
-		err = -ENOMEM;
-		goto fail;
+		dev_err(dev, "Can't allocate memory\n");
+		return -ENOMEM;
 	}
 	platform_set_drvdata(pdev, pdev_info);
 
@@ -161,16 +184,32 @@ static int sam_uio_probe(struct platform_device *pdev)
 		goto fail_uio;
 	}
 	eip_pdev = of_find_device_by_node(eip_node);
-	if (!eip_pdev)
+	if (!eip_pdev) {
+		err = -EINVAL;
+		dev_err(dev, "Device for node %s is not found\n", eip_node->full_name);
 		goto fail_uio;
+	}
+	/* Enable clock for EIP197/97 device if not enabled by crypto_safexcel.ko */
+	pdev_info->clk = of_clk_get(eip_node, 0);
+	if (!IS_ERR(pdev_info->clk)) {
+		err = clk_prepare_enable(pdev_info->clk);
+		if (err) {
+			dev_err(dev, "unable to enable clk (%d)\n", err);
+			goto fail_uio;
+		}
+	}
 
 	res = platform_get_resource(eip_pdev, IORESOURCE_MEM, 0);
-	if (!res)
+	if (!res) {
+		err = -EINVAL;
+		dev_err(dev, "failed to get resource\n");
 		goto fail_uio;
+	}
 
 	pdev_info->regs_vbase = devm_ioremap(dev, res->start, resource_size(res));
 	if (IS_ERR(pdev_info->regs_vbase)) {
-		dev_err(dev, "failed to get resource\n");
+		err = -EINVAL;
+		dev_err(dev, "failed to to ioremap\n");
 		goto fail_uio;
 	}
 
@@ -179,6 +218,7 @@ static int sam_uio_probe(struct platform_device *pdev)
 	else if (!strcmp(eip_node->name, "eip97"))
 		xdr_regs_vbase = pdev_info->regs_vbase + SAM_EIP97_xDR_REGS_OFFS;
 	else {
+		err = -EINVAL;
 		dev_err(dev, "unknown HW type\n");
 		goto fail_uio;
 	}
@@ -188,7 +228,7 @@ static int sam_uio_probe(struct platform_device *pdev)
 					GFP_KERNEL);
 	if (!pdev_info->rings) {
 		err = -ENOMEM;
-		goto fail;
+		goto fail_uio;
 	}
 
 	snprintf(pdev_info->name, sizeof(pdev_info->name), "uio_%s_%d",  eip_node->name, cpn_count);
@@ -198,9 +238,13 @@ static int sam_uio_probe(struct platform_device *pdev)
 
 		uio = &ring_info->uio;
 
-		snprintf(ring_info->ring_name, sizeof(ring_info->ring_name), "%s:%d",  pdev_info->name, idx);
 		ring_info->ring = idx;
 		ring_info->xdr_regs_vbase = xdr_regs_vbase + SAM_RING_OFFS(ring_info->ring);
+		/* Check how many rings already in use */
+		if (sam_uio_ring_is_busy(ring_info))
+			continue;
+
+		snprintf(ring_info->ring_name, sizeof(ring_info->ring_name), "%s:%d",  pdev_info->name, idx);
 		uio->name = ring_info->ring_name;
 		uio->version = DRIVER_VERSION;
 
@@ -222,19 +266,22 @@ static int sam_uio_probe(struct platform_device *pdev)
 
 		err = uio_register_device(dev, uio);
 		if (err) {
-			dev_err(dev, "Failed to register uio device\n");
-			goto fail_uio;
+			dev_warn(dev, "Failed to register uio device\n");
+			memset(ring_info, 0, sizeof(*ring_info));
+			continue;
 		}
+		pdev_info->rings_map |= BIT(idx);
 		pdev_info->uio_num++;
 	}
-	dev_info(dev, "Registered %d uio devices\n", pdev_info->uio_num);
+	dev_info(dev, "Registered %d uio devices, available rings map 0x%x\n",
+		pdev_info->uio_num, pdev_info->rings_map);
 
 	cpn_count++;
 	return 0;
 
 fail_uio:
 	sam_uio_remove(pdev);
-fail:
+
 	return err;
 }
 
@@ -250,11 +297,21 @@ static int sam_uio_remove(struct platform_device *pdev)
 		return -EINVAL;
 
 	if (pdev_info->uio_num != 0) {
-		for (int idx = 0; idx < pdev_info->uio_num; idx++)
-			uio_unregister_device(&pdev_info->rings[idx].uio);
+		for (int idx = 0; idx < pdev_info->hw_rings_num; idx++) {
+			if (pdev_info->rings_map & BIT(idx)) {
+				sam_uio_reset_ring(&pdev_info->rings[idx]);
+				uio_unregister_device(&pdev_info->rings[idx].uio);
+			}
+		}
 	}
-	devm_kfree(&pdev->dev, pdev_info->rings);
+	if (pdev_info->rings)
+		devm_kfree(&pdev->dev, pdev_info->rings);
+
+	if (pdev_info->clk)
+		clk_disable_unprepare(pdev_info->clk);
+
 	devm_kfree(&pdev->dev, pdev_info);
+
 	platform_set_drvdata(pdev, NULL);
 
 	return 0;
