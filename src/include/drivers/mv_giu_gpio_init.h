929343a67275b621d2d6b0fb1711d7527bce26b5
orenbh <orenbh@marvell.com>
2017-12-11 15:32:15 +0200

    mng: implement giu driver init sequence
    Porting of giu init sequence from nic-pf
    
    Change-Id: I27e67a58208fac1e1495663cebd733fc9a22a578
    Signed-off-by: orenbh <orenbh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/47512
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Arie Elias <earie@marvell.com>
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>

---
+++ src/include/drivers/mv_giu_gpio_init.h
@@ -0,0 +1,111 @@
+/******************************************************************************
+ *	Copyright (C) 2017 Marvell International Ltd.
+ *
+ *  If you received this File from Marvell, you may opt to use, redistribute
+ *  and/or modify this File under the following licensing terms.
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *	* Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ *
+ *	* Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the distribution.
+ *
+ *	* Neither the name of Marvell nor the names of its contributors may be
+ *	  used to endorse or promote products derived from this software
+ *	  without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+#ifndef _MV_GIU_GPIO_INIT_H
+#define _MV_GIU_GPIO_INIT_H
+
+/* RSS Hash Type
+ *
+ *	HASH_2_TUPLE - IP-src, IP-dst
+ *	HASH_5_TUPLE - IP-src, IP-dst, IP-Prot, L4-src, L4-dst
+ */
+enum rss_hash_type {
+	RSS_HASH_NONE = 0,
+	RSS_HASH_2_TUPLE,
+	RSS_HASH_5_TUPLE
+
+};
+
+/* In TC - Queue topology */
+struct giu_gpio_intc_params {
+	u32 tc_id;
+
+	/* lcl_eg_tcs */
+	u32 num_inqs;
+	struct giu_gpio_q *inqs_params;
+
+	/* lcl_bm_qs_num */
+	u32 num_inpools;
+	u32 pool_buf_size;
+	/* lcl_bm_qs_params */
+	struct giu_gpio_q *pools;
+
+	/* host_eg_tcs */
+	u32 num_rem_outqs;
+	struct giu_gpio_q *rem_outqs_params;
+
+};
+
+struct giu_gpio_intcs_params {
+	u32 num_intcs;
+	struct giu_gpio_intc_params *intc_params;
+
+};
+
+/* Out TC - Queue topology */
+struct giu_gpio_outtc_params {
+	u32 tc_id;
+
+	/* lcl_ing_tcs */
+	u32 num_outqs;
+	struct giu_gpio_q *outqs_params;
+
+	/* host_ing_tcs */
+	u32 num_rem_inqs;
+	u8 rss_type;
+	struct giu_gpio_q *rem_inqs_params;
+
+	u32 host_bm_qs_num;
+	struct giu_gpio_q *rem_poolqs_params;
+
+};
+
+struct giu_gpio_outtcs_params {
+	u32 num_outtcs;
+	struct giu_gpio_outtc_params *outtc_params;
+
+};
+
+/* Queue topology */
+struct giu_gpio_init_params {
+	u8 id;
+
+	struct mqa *mqa;
+	struct gie_data *gie;
+
+	struct giu_gpio_intcs_params  intcs_params;
+	struct giu_gpio_outtcs_params outtcs_params;
+
+};
+
+#endif /* _MV_GIU_GPIO_INIT_H */
+
