346be1f3f56299eac8de81db47fcb5b1b90c5c02
Alan Winkowski <walan@marvell.com>
2017-12-20 16:59:40 +0200

    mng: add nmp_guest API and update nmp API
    - For NMP API, a backwards compatibility API remains in
      the code until all apps are updated to the new API
    
    Change-Id: I3abfb62b338fe36f50d9811e9f85adf8d493e19e
    Signed-off-by: Alan Winkowski <walan@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/47901
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Shlomi Gridish <sgridish@marvell.com>
    Reviewed-by: Yuval Caduri <cyuval@marvell.com>

--- src/include/mng/mv_nmp.h
+++ src/include/mng/mv_nmp.h
@@ -41,27 +41,129 @@
  *  @{
  */
 
+#define NMP_LF_MAX_NUM_LCL_BPOOLS	3
+#define NMP_LF_MAX_NUM_CONTAINERS	4
+#define NMP_LF_MAX_NUM_LFS		8
+#define NMP_MAX_CMD_MSG_SIZE		128
+
 /* nmp handler declaration */
 struct nmp;
 
 /* nmp parameters definition */
 
-/* nmp logical functional type */
+/**
+ * nmp logical function types
+ *
+ */
 enum nmp_lf_type {
-	NMP_LF_T_NONE = 0,
-	NMP_LF_T_PF,
-	NMP_LF_T_VF,
-	NMP_LF_T_LAST,
+	NMP_LF_T_NIC_NONE = 0,
+	NMP_LF_T_NIC_PF,		/**< Logical function of type physcal function */
+	NMP_LF_T_NIC_VF,		/**< Logical function of type virtual function */
+	NMP_LF_T_NIC_LAST
 };
 
-/* nmp scheduling options */
+/**
+ * nmp physical function nic types
+ *
+ */
+enum nmp_lf_nicpf_type {
+	NMP_LF_NICPF_T_NONE = 0,
+	NMP_LF_NICPF_T_PP2_PORT,	/**< nic_pf of type PP2_PORT */
+	NMP_LF_NICPF_T_PP2_LAG,		/**< nic_pf of type PP2_LAG */
+	NMP_LF_NICPF_T_LAST
+};
+
+/**
+ * NMP pp2 parameters structure
+ *
+ */
+struct nmp_pp2_params {
+	u16 bm_pool_reserved_map;	/**< pp2 bpool reserved map */
+};
+
+/**
+ * NMP bpool parameters structure
+ *
+ */
+struct nmp_lf_bpool_params {
+	u16 max_num_buffs;		/**< maximum number of buffers in the bpool */
+	u16 buff_size;			/**< bpool buffer size */
+};
+
+/**
+ * NMP pp2 port parameters structure
+ *
+ */
+struct nmp_lf_nicpf_pp2_port_params {
+	char *match;			/**< matching ppio name */
+	u8 lcl_num_bpools;		/**< number of pools in pp2 ppio */
+	/** local bpools parameters for each bpool in the ppio */
+	struct nmp_lf_bpool_params lcl_bpools_params[NMP_LF_MAX_NUM_LCL_BPOOLS];
+};
+
+/**
+ * NMP logical function structure
+ *
+ * TODO: rename to .nmp_lf_nicpf_params. Left for backwards compatibility until API is updated in all apps
+ */
+struct nmp_lf_nicpf_params_new {
+	int pci_en;				/**< Flag inidicating PCI interface is present*/
+	u16 lcl_egress_qs_size;			/**< local egress queue size */
+	u16 lcl_ingress_qs_size;		/**< local ingress queue size */
+	u16 dflt_pkt_offset;			/**< default packet offset */
+	u8 max_num_tcs;				/**< maximum number of TC's */
+	u8 lcl_num_bpools;			/**< local number of pools for GIU*/
+	/** local bpools parameters for each bpool in GIU */
+	struct nmp_lf_bpool_params lcl_bpools_params[NMP_LF_MAX_NUM_LCL_BPOOLS];
+	enum nmp_lf_nicpf_type type;		/**< Type of nic pf (pp2_port, pp2_lag, etc) */
+	union {
+		/** nic physical function pp2_port parameters */
+		struct nmp_lf_nicpf_pp2_port_params pp2_port;
+	} port_params;
+};
+
+/**
+ * NMP logical function structure
+ *
+ * TODO: rename to .nmp_lf_params. Left for backwards compatibility until API is updated in all apps
+ */
+struct nmp_lf_params_new {
+	enum nmp_lf_type type;		/**< Type of logical function (Virtual function, physcal function, etc)*/
+	union {
+		/** Logical function layer parameters for Physical Function type */
+		struct nmp_lf_nicpf_params_new nicpf;
+	} u;
+};
+
+/**
+ * NMP container structure
+ *
+ */
+struct nmp_container_params {
+	u8 num_lfs;		/**< Number of NMP logical function layers*/
+	/** Parameters relevant to the initialization of the NMP logical function layer
+	 * including MUSDK networking drivers like MVPP2, MVSAM, MVGIU)
+	 */
+	struct nmp_lf_params_new *lfs_params;
+	u8 guest_id;		/**< entity with visibility to all the container's data and cntrl traffic */
+				/**< if no guest are using this container, use ‘0’; other wise, use >1 value */
+};
+
+/**
+ * nmp scheduling types
+ *
+ */
 enum nmp_sched_type {
 	NMP_SCHED_RX = 0,
 	NMP_SCHED_TX,
 	NMP_SCHED_MNG
 };
 
-/* nmp PF parameters */
+/**
+ * nmp PF parameters
+ *
+ * TODO: remove. Left for backwards compatibility until API is updated in all apps
+ */
 struct nmp_lf_pf_params {
 	enum nmp_lf_type type;
 	int  pci_en;
@@ -74,12 +176,25 @@ struct nmp_lf_pf_params {
 	int  lcl_bm_buf_size;
 };
 
-/* nmp union for logical functions types */
+/**
+ * nmp union for logical functions types
+ *
+ * TODO: remove. Left for backwards compatibility until API is updated in all apps
+ */
 union nmp_lf_params {
 	struct nmp_lf_pf_params pf;
 };
 
 struct nmp_params {
+	int pp2_en;		/**< Flag inidicating PP2 interface is present*/
+	/** pp2_params is used for initializing pp2 interface (pp2_init)
+	 * relevant only when pp2_en is set
+	 */
+	struct nmp_pp2_params pp2_params;
+	/** NMP may have several containers, each one representing a user process/VM/container */
+	u8 num_containers;
+	struct nmp_container_params *containers_params;
+	/* TODO: remove. Left for backwards compatibility until API is updated in all apps */
 	u8     num_lfs;
 	union nmp_lf_params *lfs_params;
 };
@@ -93,7 +208,7 @@ struct nmp_params {
 int nmp_init(struct nmp_params *params, struct nmp **nmp);
 
 /**
- *	Start NMP scheduling.
+ *	Trigger an NMP scheduling loop
  *
  *	@retval	0 on success
  *	@retval	<0 on failure
---
+++ src/include/mng/mv_nmp_guest.h
@@ -0,0 +1,165 @@
+/******************************************************************************
+ *	Copyright (C) 2017 Marvell International Ltd.
+ *
+ *  If you received this File from Marvell, you may opt to use, redistribute
+ *  and/or modify this File under the following licensing terms.
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *	* Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ *
+ *	* Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the distribution.
+ *
+ *	* Neither the name of Marvell nor the names of its contributors may be
+ *	  used to endorse or promote products derived from this software
+ *	  without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+
+#ifndef _MV_NMP_GUEST_H
+#define _MV_NMP_GUEST_H
+
+/** @addtogroup grp_nmp_init Networking Mgmt Proxy Init
+ *
+ *  Networking Management Proxy (NMP) Guest Initialization API
+ *  documentation
+ *
+ *  @{
+ */
+
+/* nmp_guest handler declaration */
+struct nmp_guest;
+
+/**
+ * NMP Guest initialization params
+ *
+ */
+struct nmp_guest_params {
+	/** the guest ID; this should be aligned with the guest-ID registered in the
+	* NMP JSON parameters file
+	*/
+	u8 id;
+	u32 timeout;		/**< timeout in mili-secs */
+};
+
+/**
+ * Initialize the NMP Guest.
+ *
+ * @param[in]	params		pointer to structure containing NMP guest config parameters.
+ * @param[out]	g		pointer to opaque guest handle of type 'struct nmp_guest *'
+ *
+ *	@retval	0 on success
+ *	@retval	<0 on failure
+ */
+int nmp_guest_init(struct nmp_guest_params *params, struct nmp_guest **g);
+
+/**
+ * De-initialize the NMP Guest.
+ *
+ * @param[in]	g		pointer to a guest handle.
+ *
+ *	@retval	0 on success
+ *	@retval	<0 on failure
+ */
+void nmp_guest_deinit(struct nmp_guest *g);
+
+/**
+ * Get the NMP guest probe string.
+ *
+ * this API should be called by the user application in order to retrieve the probing string that was extracted
+ * from the guest JSON file. the application shall use this string in order to extract and execute init calls
+ * to all registered objects (e.g. pp2-bpool, giu-gpio, etc.).
+ *
+ * @param[in]	g		pointer to a guest handle.
+ * @param[out]	prb_str		pointer to the guest string containing the serialized data.
+ *
+ *	@retval	0 on success
+ *	@retval	<0 on failure
+ */
+
+int nmp_guest_get_probe_str(struct nmp_guest *g, char **prb_str);
+
+/****************************************************************************
+ *	Run-time API
+ ****************************************************************************/
+
+#define NMP_GUEST_EV_CUSTOM 0x1LL
+#define NMP_GUEST_EV_PF_MTU 0x2LL
+#define NMP_GUEST_EV_PF_MAC_ADDR 0x3LL
+
+/**
+ * NMP Guest LF Type
+ *
+ */
+enum nmp_guest_lf_type {
+	NMP_GUEST_LF_T_NONE = 0,
+	NMP_GUEST_LF_T_PF,
+	NMP_GUEST_LF_T_LAST
+};
+
+/**
+ * Register the NMP guest event handler.
+ *
+ * this API should be called by the user application (may be called several times)
+ * in order to register events that this guest would like to listen.
+ *
+ * @param[in]	g		pointer to a guest handle.
+ * @param[in]	lf_type		LF type.
+ * @param[in]	ev_mask		event mask.
+ * @param[in]	client		dispatcher client.
+ * @param[in]	guest_ev_cb	event callback function.
+ *
+ *	@retval	0 on success
+ *	@retval	<0 on failure
+ */
+int nmp_guest_register_event_handler(struct nmp_guest *g,
+				     enum nmp_guest_lf_type lf_type,
+				     u8 lf_id,
+				     u64 ev_mask,
+				     void *client,
+				     int (*guest_ev_cb)(void *client, u8 client, u8 id, u8 code,
+							u16 indx, void *msg, u16 len));
+
+/**
+ * NMP Guest schedule.
+ *
+ * this API should be called by the guest application in order to check the guest message
+ * queue for incoming messages (inband-management commands)
+ *
+ * @param[in]	g		pointer to a guest handle.
+ *
+ *	@retval	0 on success
+ *	@retval	<0 on failure
+ */
+int nmp_guest_schedule(struct nmp_guest *g);
+
+/**
+ * NMP Guest message send.
+ *
+ * @param[in]	g		pointer to a guest handle.
+ * @param[in]	code		message code.
+ * @param[in]	indx		message index.
+ * @param[in]	msg		pointer to the message.
+ * @param[in]	len		message length.
+ *
+ *	@retval	0 on success
+ *	@retval	<0 on failure
+ */
+int nmp_guest_send_msg(struct nmp_guest *g, u8 code, u16 indx, void *msg, u16 len);
+
+#endif /* _MV_NMP_GUEST_H */
+
