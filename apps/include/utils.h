02ce1b48f9ccfdff504bdf337a28399177865cd0
Dmitri Epshtein <dima@marvell.com>
2018-01-29 15:21:30 +0200

    apps: add encrypt and decrypt counters to perf struct
    Change-Id: I88fed9bd54e28be7c0d896bdf19057ef0c3bf51d
    Signed-off-by: Dmitri Epshtein <dima@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/49721
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>

--- apps/include/utils.h
+++ apps/include/utils.h
@@ -144,6 +144,8 @@ struct glb_common_args {
 	struct timeval		ctrl_trd_last_time;
 	u64			last_rx_cnt;
 	u64			last_tx_cnt;
+	u64			last_enc_cnt;
+	u64			last_dec_cnt;
 	u32			busy_wait;
 	pthread_mutex_t		thread_lock; /* General Purpose Lock, not intended for data-path */
 	bool			shared_hifs; /* Indicates system has shared hifs. */
@@ -158,6 +160,8 @@ struct glb_common_args {
 struct perf_cmn_cntrs {
 	u64			rx_cnt;
 	u64			tx_cnt;
+	u64			enc_cnt;
+	u64			dec_cnt;
 	u64			drop_cnt;
 };
 struct local_common_args {
