4f248551e4329defa11c023b6210896a285409d6
Liron Himi <lironh@marvell.com>
2018-05-22 19:54:32 +0300

    nmp/giu: change license to marvell proprietary
    Change-Id: I0d238a3a142d60cc73ced36f5f68ca1e65310382
    Signed-off-by: Liron Himi <lironh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/55467
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Alan Winkowski <walan@marvell.com>
    Reviewed-by: Nir Erez <nerez@marvell.com>
    Reviewed-by: Neta Zur Hershkovits <neta@marvell.com>

--- src/hw_emul/gie/gie_buff_desc.h
+++ src/hw_emul/gie/gie_buff_desc.h
@@ -1,15 +1,9 @@
-/*
-** Copyright (C) 2015-2016 Marvell International Ltd.
-** This program is free software: you can redistribute it and/or
-** modify it under the terms of the GNU General Public License as
-** published by the Free Software Foundation, either version 2 of the
-** License, or any later version.
-**
-** This program is distributed in the hope that it will be useful, but
-** WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-** General Public License for more details.
-*/
+/******************************************************************************
+*  Copyright (C) 2018 Marvell International Ltd.
+*
+*  This program is provided "as is" without any warranty of any kind, and is
+*  distributed under the applicable Marvell limited use license agreement.
+*******************************************************************************/
 
 /* This file includes the definitions of all data structures that should be
  * shared with the various drivers and modules on the SNIC side.
