4bdd57e71ffc1707e2f4b7a970b84c133de0d292
Liron Himi <lironh@marvell.com>
2018-06-27 16:29:48 +0300

    apps: nmp/giu: use 'nmp_read_cfg_file'
    Change-Id: I2095cbed44174a446988602ff4b451ade2d51099
    Signed-off-by: Liron Himi <lironh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/57563
    Reviewed-by: Shlomi Gridish <sgridish@marvell.com>
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Alan Winkowski <walan@marvell.com>

--- apps/common/nmp_guest_utils.c
+++ apps/common/nmp_guest_utils.c
@@ -18,13 +18,6 @@
 #define NMP_CFG_FILE_VAR_DIR		"/var/"
 #define NMP_CFG_FILE_NAME_PREFIX	"musdk-nmp-config.txt"
 
-#define NMP_MAX_NUM_CONTAINERS		4
-#define NMP_MAX_NUM_LFS			8
-#define NMP_GIE_MAX_TCS			8
-#define NMP_GIE_MAX_Q_PER_TC		128
-#define NMP_GIE_MAX_BPOOLS		16
-#define NMP_GIE_MAX_BM_PER_Q		1
-
 /* Number of BM pools reserved by kernel */
 #define MVAPPS_PP2_NUM_BPOOLS_RSRV	3
 /* Reserved BM pools mask */
@@ -37,272 +30,34 @@
 /*  Guest mode related functions  */
 /*--------------------------*/
 
-static int nmp_range_validate(int value, int min, int max)
-{
-	if (((value) > (max)) || ((value) < (min))) {
-		pr_err("%s: value 0x%X (%d) is out of range [0x%X , 0x%X].\n",
-			__func__, (value), (value), (min), (max));
-		return -EINVAL;
-	}
-	return 0;
-}
-
 int app_read_nmp_cfg_file(char *cfg_file, struct nmp_params *params)
 {
-	u32				 i, j, k, rc;
-	char				 file_name[SER_MAX_FILE_NAME];
-	char				 buff[SER_MAX_FILE_SIZE];
-	char				*sec = NULL;
-	char				 tmp_buf[NMP_MAX_BUF_STR_LEN];
-	char				 pp2_name[NMP_MAX_BUF_STR_LEN], giu_name[NMP_MAX_BUF_STR_LEN];
-	struct nmp_lf_nicpf_params	*pf;
-	u32				 num_lfs = 0;
+	int	rc;
+	char	file_name[SER_MAX_FILE_NAME];
 
 	/* If cfg-file is provided, read the nmp-config from this location. Otherwise try to read either from
 	 * local dir or from /var dir
 	 */
 	strcpy(file_name, cfg_file);
-	rc = read_file_to_buf(file_name, buff, SER_MAX_FILE_SIZE);
+	rc = nmp_read_cfg_file(file_name, params);
 	if (rc) {
 		memset(file_name, 0, SER_MAX_FILE_NAME);
 		snprintf(file_name, sizeof(file_name), "%s%s", NMP_CFG_FILE_LOCAL_DIR, NMP_CFG_FILE_NAME_PREFIX);
-		rc = read_file_to_buf(file_name, buff, SER_MAX_FILE_SIZE);
+		rc = nmp_read_cfg_file(file_name, params);
 		if (rc) {
 			memset(file_name, 0, SER_MAX_FILE_NAME);
 			snprintf(file_name, sizeof(file_name), "%s%s", NMP_CFG_FILE_VAR_DIR, NMP_CFG_FILE_NAME_PREFIX);
-			rc = read_file_to_buf(file_name, buff, SER_MAX_FILE_SIZE);
+			rc = nmp_read_cfg_file(file_name, params);
 			if (rc) {
 				pr_info("nmp_config_file not found\n");
 				return rc;
 			}
 		}
 	}
+
 	pr_info("nmp_cfg_location: %s\n", file_name);
 
-	/* Check if there are nmp-params */
-	sec = strstr(buff, "nmp_params");
-	if (!sec) {
-		pr_err("nmp_params section not found!\n");
-		return -EINVAL;
-	}
-
-	/* Check if pp2 is enabled */
-	json_buffer_to_input(sec, "pp2_en", params->pp2_en);
-	if (nmp_range_validate(params->pp2_en, 0, 1) != 0) {
-		pr_err("pp2_en not in tange!\n");
-		return -EINVAL;
-	}
-
-	/* if pp2 enabled, set the pp2_params*/
-	if (params->pp2_en) {
-		sec = strstr(sec, "pp2_params");
-		if (!sec) {
-			pr_err("'pp2_params' not found\n");
-			return -EINVAL;
-		}
-
-		json_buffer_to_input(sec, "bm_pool_reserved_map", params->pp2_params.bm_pool_reserved_map);
-		if (nmp_range_validate(params->pp2_params.bm_pool_reserved_map, 0, PP2_BPOOL_NUM_POOLS)) {
-			pr_info("bm_pool_reserved_map not in tange!\n");
-			rc = -EINVAL;
-			goto read_cfg_exit1;
-		}
-	}
-
-	/* Read number of containers */
-	json_buffer_to_input(sec, "num_containers", params->num_containers);
-	if (nmp_range_validate(params->num_containers, 1, NMP_MAX_NUM_CONTAINERS)) {
-		pr_info("num_containers not in tange!\n");
-		rc = -EINVAL;
-		goto read_cfg_exit1;
-	}
-
-	params->containers_params = kcalloc(1, sizeof(struct nmp_container_params) *
-					    params->num_containers, GFP_KERNEL);
-	if (params->containers_params == NULL) {
-		rc = -ENOMEM;
-		goto read_cfg_exit1;
-	}
-
-	for (i = 0; i < params->num_containers; i++) {
-		memset(tmp_buf, 0, sizeof(tmp_buf));
-		snprintf(tmp_buf, sizeof(tmp_buf), "containers_params-%d", i);
-		sec = strstr(sec, tmp_buf);
-		if (!sec) {
-			pr_err("'containers_params' not found\n");
-			rc = -EINVAL;
-			goto read_cfg_exit1;
-		}
-		/* Read number of lfs */
-		json_buffer_to_input(sec, "num_lfs", params->containers_params[i].num_lfs);
-		if (nmp_range_validate(params->containers_params[i].num_lfs, 1, NMP_MAX_NUM_LFS) != 0) {
-			rc = -EINVAL;
-			goto read_cfg_exit1;
-		}
-
-		params->containers_params[i].lfs_params = kcalloc(1, sizeof(struct nmp_lf_params) *
-								params->containers_params[i].num_lfs, GFP_KERNEL);
-		if (params->containers_params[i].lfs_params == NULL) {
-			rc = -ENOMEM;
-			goto read_cfg_exit2;
-		}
-		num_lfs++;
-
-		for (j = 0; j < params->containers_params[i].num_lfs; j++) {
-			/* Read lf type*/
-			json_buffer_to_input(sec, "lf_type", params->containers_params[i].lfs_params[j].type);
-			if (nmp_range_validate(params->containers_params[i].lfs_params[j].type,
-					       NMP_LF_T_NIC_NONE, NMP_LF_T_NIC_LAST - 1) != 0) {
-				rc = -EINVAL;
-				goto read_cfg_exit2;
-			}
-
-			if (params->containers_params[i].lfs_params[j].type == NMP_LF_T_NIC_PF) {
-				/* Read nicpf*/
-				pf = (struct nmp_lf_nicpf_params *)
-				     &params->containers_params[i].lfs_params[j].u.nicpf;
-
-				pf->match = giu_name;
-				json_buffer_to_input_str(sec, "match", pf->match);
-
-				pf->keep_alive_thresh = (u32)-1;
-				json_buffer_to_input(sec, "keep_alive_thresh", pf->keep_alive_thresh);
-				if (pf->keep_alive_thresh == (u32)-1) {
-					pr_err("missing keep_alive_thresh!\n");
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				json_buffer_to_input(sec, "pci_en", pf->pci_en);
-				if (nmp_range_validate(pf->pci_en, 0, 1) != 0) {
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				json_buffer_to_input(sec, "lcl_egress_qs_size", pf->lcl_egress_qs_size);
-				if (!pf->lcl_egress_qs_size) {
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				json_buffer_to_input(sec, "lcl_ingress_qs_size", pf->lcl_ingress_qs_size);
-				if (!pf->lcl_ingress_qs_size) {
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				json_buffer_to_input(sec, "dflt_pkt_offset", pf->dflt_pkt_offset);
-				if (nmp_range_validate(pf->dflt_pkt_offset, 0, 1024) != 0) {
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				json_buffer_to_input(sec, "max_num_tcs", pf->max_num_tcs);
-				if (nmp_range_validate(pf->max_num_tcs, 0, NMP_GIE_MAX_TCS) != 0) {
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				json_buffer_to_input(sec, "lcl_num_bpools", pf->lcl_num_bpools);
-				if (nmp_range_validate(pf->lcl_num_bpools, 0, NMP_GIE_MAX_BPOOLS) != 0) {
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				for (k = 0; k < pf->lcl_num_bpools; k++) {
-					memset(tmp_buf, 0, sizeof(tmp_buf));
-					snprintf(tmp_buf, sizeof(tmp_buf), "lcl_bpools_params-%d", k);
-					sec = strstr(sec, tmp_buf);
-					if (!sec) {
-						pr_err("'lcl_bpools_params' not found\n");
-						rc = -EINVAL;
-						goto read_cfg_exit2;
-					}
-
-					json_buffer_to_input(sec, "max_num_buffs",
-							     pf->lcl_bpools_params[k].max_num_buffs);
-					if (!pf->lcl_bpools_params[k].max_num_buffs) {
-						rc = -EINVAL;
-						goto read_cfg_exit2;
-					}
-
-					json_buffer_to_input(sec, "buff_size", pf->lcl_bpools_params[k].buff_size);
-					if (!pf->lcl_bpools_params[k].buff_size) {
-						rc = -EINVAL;
-						goto read_cfg_exit2;
-					}
-				}
-
-				json_buffer_to_input(sec, "nicpf_type", pf->type);
-				if (nmp_range_validate(pf->type, NMP_LF_NICPF_T_NONE,
-						       NMP_LF_NICPF_T_LAST - 1) != 0) {
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				if (pf->type != NMP_LF_NICPF_T_PP2_PORT) {
-					rc = -EINVAL;
-					goto read_cfg_exit2;
-				}
-
-				sec = strstr(sec, "port-params-pp2-port");
-				if (!sec) {
-					pr_err("'port-params-pp2-port' not found\n");
-					return -EINVAL;
-				}
-
-				pf->port_params.pp2_port.match = pp2_name;
-				json_buffer_to_input_str(sec, "match", pf->port_params.pp2_port.match);
-				if (!pf->port_params.pp2_port.match) {
-					pr_err("'pp2 match' not found\n");
-					return -EINVAL;
-				}
-
-				json_buffer_to_input(sec, "lcl_num_bpools", pf->port_params.pp2_port.lcl_num_bpools);
-				if (nmp_range_validate(pf->port_params.pp2_port.lcl_num_bpools,
-						       1, NMP_LF_MAX_NUM_LCL_BPOOLS) != 0)
-					return -EINVAL;
-
-				for (k = 0; k < pf->port_params.pp2_port.lcl_num_bpools; k++) {
-					struct nmp_lf_bpool_params *lcl_bpools_params =
-						&pf->port_params.pp2_port.lcl_bpools_params[k];
-
-					memset(tmp_buf, 0, sizeof(tmp_buf));
-					snprintf(tmp_buf, sizeof(tmp_buf), "lcl_bpools_params-%d", k);
-					sec = strstr(sec, tmp_buf);
-					if (!sec) {
-						pr_err("'lcl_bpools_params' not found\n");
-						return -EINVAL;
-					}
-
-					json_buffer_to_input(sec, "max_num_buffs",
-							lcl_bpools_params->max_num_buffs);
-					if (nmp_range_validate(pf->port_params.pp2_port.lcl_num_bpools,
-							       0, 4096) != 0) {
-						return -EINVAL;
-					}
-
-					json_buffer_to_input(sec, "buff_size", lcl_bpools_params->buff_size);
-					if (nmp_range_validate(lcl_bpools_params->buff_size, 0, 4096) != 0)
-						return -EINVAL;
-				}
-			}
-		}
-
-		json_buffer_to_input(sec, "guest_id", params->containers_params[i].guest_id);
-		if (nmp_range_validate(params->containers_params[i].guest_id, 0, 10) != 0) {
-			rc = -EINVAL;
-			goto read_cfg_exit2;
-		}
-	}
-
 	return 0;
-read_cfg_exit2:
-	for (i = 0; i < num_lfs; i++)
-		kfree(params->containers_params[i].lfs_params);
-read_cfg_exit1:
-	kfree(params->containers_params);
-	return rc;
 }
 
 int app_guest_utils_build_all_bpools(char *buff, struct nmp_guest_info *guest_info,
