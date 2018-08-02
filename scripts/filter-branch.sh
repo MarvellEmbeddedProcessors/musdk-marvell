3045ea2f4fb75d9f218b7b43ee996a352dd73552
Liron Himi <lironh@marvell.com>
2018-08-02 15:11:36 +0300

    scripts: add filter-branch.sh script
    Remove clean-branch script since it will be maintained
    in CI
    
    Change-Id: Ic87aa1de96aef2ca7aa35f2f7f3727b6f217b12a
    Signed-off-by: Liron Himi <lironh@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/58650
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>

---
+++ scripts/filter-branch.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Run filter-branch to remove proprietary files
+time git filter-branch --force --index-filter '\
+git rm -rf --cached --ignore-unmatch src/hw_emul \
+git rm -rf --cached --ignore-unmatch src/mng \
+git rm -rf --cached --ignore-unmatch src/drivers/giu \
+git rm -rf --cached --ignore-unmatch src/drivers/mqa \
+git rm -rf --cached --ignore-unmatch src/include/hw_emul \
+git rm -rf --cached --ignore-unmatch src/include/mng \
+git rm -rf --cached --ignore-unmatch src/include/drivers/giu_regfile_def.h \
+git rm -rf --cached --ignore-unmatch src/include/drivers/mv_giu_bpool.h \
+git rm -rf --cached --ignore-unmatch src/include/drivers/mv_giu_gpio.h \
+git rm -rf --cached --ignore-unmatch src/include/drivers/mqa_def.h \
+git rm -rf --cached --ignore-unmatch src/include/drivers/mv_mqa.h \
+git rm -rf --cached --ignore-unmatch src/include/drivers/mv_mqa_queue.h \
+git rm -rf --cached --ignore-unmatch apps/common/giu_utils.c \
+git rm -rf --cached --ignore-unmatch apps/common/nmp_guest_utils.c \
+git rm -rf --cached --ignore-unmatch apps/include/giu_utils.h \
+git rm -rf --cached --ignore-unmatch apps/include/nmp_guest_utils.h \
+git rm -rf --cached --ignore-unmatch apps/examples/giu \
+git rm -rf --cached --ignore-unmatch apps/examples/nmp \
+git rm -rf --cached --ignore-unmatch apps/tests/giu \
+git rm -rf --cached --ignore-unmatch doc/musdk_giu_user_guide.txt \
+git rm -rf --cached --ignore-unmatch doc/musdk_mqa_user_guide.txt \
+git rm -rf --cached --ignore-unmatch doc/musdk_nmp_user_guide.txt \
+git rm -rf --cached --ignore-unmatch scripts/filter-branch.sh \
+' --prune-empty @
