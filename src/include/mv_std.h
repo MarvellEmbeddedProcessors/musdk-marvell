5ebddb7cf435609fecdfb13d8b5b75b85ef3021a
Shlomi Gridish <sgridish@marvell.com>
2018-11-11 11:22:59 +0200

    env: mv_std: add autogen header files into the main include
    Add 'autogen' header files in mv_std.h file so it will be
    visible to all MUSDK (DPDK, MUSAF, etc.) applications
    
    Change-Id: If853cafdedb1e94a3425f91dae76abdc36cf07c9
    Signed-off-by: Shlomi Gridish <sgridish@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/60826
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Alan Winkowski <walan@marvell.com>
    Reviewed-by: Liron Himi <lironh@marvell.com>

--- src/include/mv_std.h
+++ src/include/mv_std.h
@@ -97,19 +97,25 @@
 #ifndef __MV_STD_H__
 #define __MV_STD_H__
 
+/* MUSDK primary build flags. must be at the top */
+#include "env/mv_autogen_build_assert.h"
+#include "env/mv_autogen_comp_flags.h"
+
+/* MUSDK enviromental related headers */
 #ifdef __KERNEL__
+/* MUSDK should have been compliant with Linux Kernel;
+ * so, there should not be any include files here
+ */
 
 #else /* __KERNEL__ */
-#include "env/mv_autogen_build_assert.h"
 #include "env/mv_types.h"
 #include "env/mv_compiler.h"
 #include "env/mv_common.h"
-
 #include "env/mv_errno.h"
 #include "env/mv_debug.h"
-
 #endif /* __KERNEL__ */
 
+/* MUSDK specific (but general) header files */
 #include "env/mv_sys_dma.h"
 
 #endif /* __MV_STD_H__ */
