96b6969a90867576a3a8d5dd6a320b31241a8a13
Lior Levin <levin@marvell.com>
2019-03-14 16:55:27 +0200

    scripts: add ci compile and pack scripts
    Change-Id: Ic4b84e458f898c04f2a68e6259bebe98824cf984
    Signed-off-by: Lior Levin <levin@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/5701
    Tested-by: sa_ip-sw-jenkins
    Reviewed-by: Liron Himi <liron.himi@marvell.com>

---
+++ scripts/ci/compile.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+# Copyright (C) 2018 Marvell International Ltd.
+#
+# SPDX-License-Identifier:           GPL-2.0
+# https://spdx.org/licenses
+###############################################################################
+## This is the compile script for musdk                                      ##
+## This script is called by CI automated builds                              ##
+## It may also be used interactively by users to compile the same way as CI  ##
+###############################################################################
+## WARNING: Do NOT MODIFY the CI wrapper code segments.                      ##
+## You can only modify the config and compile commands                       ##
+###############################################################################
+
+
+## =v=v=v=v=v=v=v=v=v=v=v CI WRAPPER - Do not Modify! v=v=v=v=v=v=v=v=v=v=v= ##
+set -euo pipefail
+shopt -s extglob
+##==================================== USAGE ================================##
+function usage {
+	echo """
+Usage: compile [--no_configure] [--echo_only] BUILD_NAME
+ or:   compile --help
+
+Compiles musdk similar to the given CI build
+
+ -e, --echo_only      Print out the compilation sequence but do not execute it
+ -h, --help           Display this help and exit
+
+Prerequisites:
+  CROSS_COMPILE       path to cross compiler
+  KDIR                path to kernel source
+  CDAL_PATH           (optional) path to cdal source
+"""
+	exit 0
+}
+##============================ PARSE ARGUMENTS ==============================##
+TEMP=`getopt -a -o eh --long echo_only,help \
+             -n 'compile' -- "$@"`
+
+if [ $? != 0 ] ; then
+	echo "Error: Failed parsing command options" >&2
+	exit 1
+fi
+eval set -- "$TEMP"
+
+no_configure=
+echo_only=
+
+while true; do
+	case "$1" in
+		-N | --no_configure ) no_configure=true; shift ;;
+		-e | --echo_only )    echo_only=true; shift ;;
+		-h | --help )         usage; ;;
+		-- ) shift; break ;;
+		* ) break ;;
+	esac
+done
+
+[[ $# -ne 1 ]] && usage
+build_name=$1
+
+echo "running compile.sh ${build_name}"
+## =^=^=^=^=^=^=^=^=^=^=^=^  End of CI WRAPPER code -=^=^=^=^=^=^=^=^=^=^=^= ##
+
+
+########################### MUSDK CONFIGURATION ##############################
+
+modules=(cma dmax2)
+flags=""
+
+if [[ ! $build_name =~ linux414 ]]; then
+	echo "Error: Build $build_name not supported"; exit -1;
+fi
+
+if [[ $build_name =~ _nosam ]]; then
+	sam_flag='--enable-sam=no'
+else
+	modules[${#modules[@]}]="sam"
+	sam_flag='--enable-sam'
+fi
+
+if [[ $build_name =~ _ks ]]; then
+	echo "Error: Build $build_name is currently not supported"; exit -1;
+	modules=()
+	sam_flag='--enable-sam=no'
+fi
+
+if [[ $build_name =~ _prv ]]; then
+	modules[${#modules[@]}]="gnic"
+	flags="--enable-giu"
+fi
+
+fpic=' EXTRA_CFLAGS="-fPIC"' ## assume odp-musdk version >= 1.19
+
+###############################################################################
+
+
+## =v=v=v=v=v=v=v=v=v=v=v CI WRAPPER - Do not Modify! v=v=v=v=v=v=v=v=v=v=v= ##
+cmd="""
+set -x
+pwd"""
+## =^=^=^=^=^=^=^=^=^=^=^=^  End of CI WRAPPER code -=^=^=^=^=^=^=^=^=^=^=^= ##
+
+
+#################################### COMPILE ##################################
+cmd=$cmd"""
+export ARCH=arm64"""
+
+if [[ ${#modules[@]} > 0 ]]; then
+for m in "${modules[@]}"; do
+cmd=$cmd"""
+cd modules/${m}
+make
+cd ../..
+"""
+done
+fi
+
+cmd=$cmd"""
+./bootstrap
+./configure --prefix=/usr/local/musdk ${sam_flag} ${flags} --host=aarch64-linux-gnu \
+    CC=\${CROSS_COMPILE}gcc --enable-static --disable-shared
+"""
+
+if [[ $build_name =~ _ks ]]; then
+cmd=$cmd"""
+cd build/lnx4.4_a7k_gcc
+make -C \$KDIR M=\$PWD modules -j4
+cd ../../apps/examples/ppv2/ks
+make -C \$KDIR M=\$PWD modules -j4
+cd ../../../..
+sudo make install"""
+else
+cmd=$cmd"""
+make -j4 $fpic
+sudo make install"""
+fi
+###############################################################################
+
+
+## =v=v=v=v=v=v=v=v=v=v=v CI WRAPPER - Do not Modify! v=v=v=v=v=v=v=v=v=v=v= ##
+if [[ $echo_only ]]; then
+	echo "$cmd"
+	exit 0
+fi
+
+eval "$cmd"
+## =^=^=^=^=^=^=^=^=^=^=^=^  End of CI WRAPPER code -=^=^=^=^=^=^=^=^=^=^=^= ##
---
+++ scripts/ci/pack.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# SPDX-License-Identifier:           GPL-2.0
+# https://spdx.org/licenses
+# Copyright (C) 2018 Marvell International Ltd.
+#
+###############################################################################
+## This is the pack script for linux kernel                                  ##
+## This script is called by CI automated builds                              ##
+###############################################################################
+## WARNING: Do NOT MODIFY the CI wrapper code segments.                      ##
+## You can only modify the config and compile commands                       ##
+###############################################################################
+## Prerequisites:       DESTDIR is the path to the destination directory
+## Usage:               pack BUILD_NAME
+
+## =v=v=v=v=v=v=v=v=v=v=v CI WRAPPER - Do not Modify! v=v=v=v=v=v=v=v=v=v=v= ##
+set -exuo pipefail
+shopt -s extglob
+
+build_name=$1
+echo "running pack.sh ${build_name}"
+## =^=^=^=^=^=^=^=^=^=^=^=^  End of CI WRAPPER code -=^=^=^=^=^=^=^=^=^=^=^= ##
+
+mkdir -p $DESTDIR
+mkdir -p $DESTDIR/rootfs/lib/modules/musdk
+
+modules=(cma dmax2 sam gnic)
+ko=(musdk_cma.ko mv_dmax2_uio.ko mv_sam_uio.ko mgnic_plat.ko)
+
+for i in `seq 0 $( expr ${#modules[@]} - 1 )`; do
+	kofile=modules/${modules[$i]}/${ko[$i]}
+	if [[ -e $kofile ]]; then
+		cp $kofile $DESTDIR/rootfs/lib/modules/musdk/
+	fi
+done
+
+mkdir -p ${DESTDIR}/rootfs/usr/local
+cp -R /usr/local/musdk ${DESTDIR}/rootfs/usr/local/
+
+echo "musdk pack completed"
