b5733200d5725bd1cd165108a4329995081eedb3
yuric <yuric@marvell.com>
2019-06-23 18:59:39 +0300

    pp2: port: remove port link status show
    Change-Id: Ib7fc5f949f371d272b29240652c0dbd72693355c
    Signed-off-by: yuric <yuric@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/11483
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Liron Himi <lironh@marvell.com>

--- src/drivers/ppv2/pp2_port.c
+++ src/drivers/ppv2/pp2_port.c
@@ -1214,11 +1214,6 @@ pp2_port_start(struct pp2_port *port, pp2_traffic_mode t_mode) /* Open from slow
 		pp2_port_set_enable(port, 1);
 	mdelay(500);
 	pp2_port_start_dev(port);
-
-	if (NOT_LPBK_PORT(port)) {
-		/* Sleep to allow link to come up before showing status */
-		pp2_gop_status_show(&port->parent->hw.gop, &port->mac_data);
-	}
 }
 
 /* Internal.
