76c35089eeadeed7a5bc3606f8b11ab580f35fd0
Liron Himi <lironh@marvell.com>
2019-12-25 01:30:14 +0200

    mng: lf_mng: rearrange towards vf support
    Change-Id: I34fb8f29cd98e02171f973f30bdc98463dc61d80
    Signed-off-by: Liron Himi <lironh@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/20786
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Yuri Chipchev <yuric@marvell.com>

--- src/mng/lf/lf_mng.c
+++ src/mng/lf/lf_mng.c
@@ -17,8 +17,8 @@
 #include "lf_mng.h"
 
 
-#define LF_MNG_MAX_NUM_CONTAINERS	2
-#define LF_MNG_MAX_NUM_CONTAINER_LFS	8
+#define LF_MNG_MAX_NUM_CONTAINERS	NMP_MAX_NUM_CONTAINERS
+#define LF_MNG_MAX_NUM_CONTAINER_LFS	NMP_MAX_NUM_LFS
 
 #define SER_FILE_VAR_DIR		"/var/"
 #define SER_FILE_NAME_PREFIX		"musdk-serial-cfg"
@@ -35,6 +35,7 @@ struct lf_mng_lf {
 };
 
 struct lf_mng_container {
+	struct lf_mng		*lf_mng;
 	u8			 guest_id;
 	u8			 master_id;
 
@@ -59,24 +60,22 @@ struct lf_mng {
 };
 
 
-static int lookup_lf(struct lf_mng *lf_mng, u8 lf_id, u8 *cntr, u8 *lf)
+static int lookup_lf(struct lf_mng_container *lf_mng_cont, u8 lf_id, struct lf_mng_lf **lf)
 {
-	int			 _cntr, _lf;
+	int	_lf;
 
-	for (_cntr = 0; _cntr < lf_mng->num_containers; _cntr++)
-		for (_lf = 0; _lf < lf_mng->containers[_cntr].num_lfs; _lf++)
-			if (lf_mng->containers[_cntr].lfs[_lf].id == lf_id) {
-				*cntr = _cntr;
-				*lf = _lf;
-				return 0;
-			}
+	for (_lf = 0; _lf < lf_mng_cont->num_lfs; _lf++)
+		if (lf_mng_cont->lfs[_lf].id == lf_id) {
+			*lf = &lf_mng_cont->lfs[_lf];
+			return 0;
+		}
 
 	return -ENOENT;
 }
 
 static int lf_get_free_bar(void *arg, void **va, dma_addr_t *pa)
 {
-	struct lf_mng	*lf_mng = (struct lf_mng *)arg;
+	struct lf_mng	*lf_mng = ((struct lf_mng_container *)arg)->lf_mng;
 
 	if (!lf_mng) {
 		pr_err("no LF-MNG obj!\n");
@@ -88,7 +87,7 @@ static int lf_get_free_bar(void *arg, void **va, dma_addr_t *pa)
 
 static void lf_put_bar(void *arg, int index)
 {
-	struct lf_mng	*lf_mng = (struct lf_mng *)arg;
+	struct lf_mng	*lf_mng = ((struct lf_mng_container *)arg)->lf_mng;
 
 	if (!lf_mng) {
 		pr_err("no LF-MNG obj!\n");
@@ -100,7 +99,7 @@ static void lf_put_bar(void *arg, int index)
 
 static int lf_pp_find_free_bpool(void *arg, u32 pp_id)
 {
-	struct lf_mng	*lf_mng = (struct lf_mng *)arg;
+	struct lf_mng	*lf_mng = ((struct lf_mng_container *)arg)->lf_mng;
 
 	if (!lf_mng) {
 		pr_err("no LF-MNG obj!\n");
@@ -112,31 +111,31 @@ static int lf_pp_find_free_bpool(void *arg, u32 pp_id)
 
 static int lf_init_done(void *arg, u8 lf_id)
 {
-	struct lf_mng			*lf_mng = (struct lf_mng *)arg;
+	struct lf_mng_container		*lf_mng_cont = (struct lf_mng_container *)arg;
 	struct mv_sys_dma_mem_info	 mem_info;
 	char	 file_name[SER_MAX_FILE_NAME];
 	char	 buff[SER_MAX_FILE_SIZE];
 	u32	 size = SER_MAX_FILE_SIZE;
 	char	 dev_name[100];
 	size_t	 pos = 0;
-	u8	 cntr, lf;
+	struct lf_mng_lf *lf;
 	int	 ret;
 
-	if (!lf_mng) {
-		pr_err("no LF-MNG obj!\n");
+	if (!lf_mng_cont) {
+		pr_err("no LF-MNG-container obj!\n");
 		return -EFAULT;
 	}
 
 	pr_debug("nmp_pf_init_done reached\n");
 
-	ret = lookup_lf(lf_mng, lf_id, &cntr, &lf);
+	ret = lookup_lf(lf_mng_cont, lf_id, &lf);
 	if (ret) {
 		pr_err("LF %d not found!\n", lf_id);
 		return -EIO;
 	}
 
-	if ((lf_mng->containers[cntr].lfs[lf].type != NMP_LF_T_NIC_PF) ||
-		(lf_mng->containers[cntr].lfs[lf].id != 0)) {
+	if ((lf->type != NMP_LF_T_NIC_PF) ||
+		(lf->id != 0)) {
 		pr_err("temporary only single NICPF is supported!\n");
 		return -EIO;
 	}
@@ -147,7 +146,7 @@ static int lf_init_done(void *arg, u8 lf_id)
 		"%s%s%d",
 		SER_FILE_VAR_DIR,
 		SER_FILE_NAME_PREFIX,
-		lf_mng->containers[cntr].guest_id);
+		lf_mng_cont->guest_id);
 	/* Remove the serialize files */
 	remove(file_name);
 
@@ -164,9 +163,9 @@ static int lf_init_done(void *arg, u8 lf_id)
 	json_print_to_buffer(buff, size, 2, "\"phys_addr\": 0x%lx\n", (u64)mem_info.paddr);
 	json_print_to_buffer(buff, size, 1, "},\n");
 
-	pr_debug("starting serialization of guest %d\n", lf_mng->containers[cntr].guest_id);
+	pr_debug("starting serialization of guest %d\n", lf_mng_cont->guest_id);
 
-	ret = nmnicpf_serialize(lf_mng->containers[cntr].lfs[lf].u.nmnicpf, &buff[pos], size - pos);
+	ret = nmnicpf_serialize(lf->u.nmnicpf, &buff[pos], size - pos);
 	if (ret >= 0)
 		pos += ret;
 	if (pos != strlen(buff)) {
@@ -174,8 +173,8 @@ static int lf_init_done(void *arg, u8 lf_id)
 		return -EFAULT;
 	}
 
-	if (lf_mng->containers[cntr].nmcstm) {
-		ret = nmcstm_serialize(lf_mng->containers[cntr].nmcstm, &buff[pos], size - pos);
+	if (lf_mng_cont->nmcstm) {
+		ret = nmcstm_serialize(lf_mng_cont->nmcstm, &buff[pos], size - pos);
 		if (ret >= 0)
 			pos += ret;
 		if (pos != strlen(buff)) {
@@ -189,7 +188,7 @@ static int lf_init_done(void *arg, u8 lf_id)
 	/* write buffer to file */
 	ret = write_buf_to_file(file_name, buff, strlen(buff));
 	if (ret) {
-		pr_err("Failed to write to guest %d file\n", lf_mng->containers[cntr].guest_id);
+		pr_err("Failed to write to guest %d file\n", lf_mng_cont->guest_id);
 		return ret;
 	}
 
@@ -227,6 +226,7 @@ int lf_mng_init(struct lf_mng_params *params, struct lf_mng **lf_mng)
 
 	_lf_mng->num_containers = params->num_containers;
 	for (cntr = 0; cntr < _lf_mng->num_containers; cntr++) {
+		_lf_mng->containers[cntr].lf_mng = _lf_mng;
 		_lf_mng->containers[cntr].guest_id = params->containers_params[cntr].guest_id;
 		/* We assume the first LF in the container is the "master" */
 		_lf_mng->containers[cntr].master_id = _lf_mng->total_num_nicpfs;
@@ -255,7 +255,7 @@ int lf_mng_init(struct lf_mng_params *params, struct lf_mng **lf_mng)
 				nmnicpf_params.f_get_free_bar_cb = lf_get_free_bar;
 				nmnicpf_params.f_put_bar_cb = lf_put_bar;
 				nmnicpf_params.f_pp_find_free_bpool_cb = lf_pp_find_free_bpool;
-				nmnicpf_params.arg = _lf_mng;
+				nmnicpf_params.arg = &_lf_mng->containers[cntr];
 
 				err = nmnicpf_init(&nmnicpf_params,
 					&_lf_mng->containers[cntr].lfs[lf].u.nmnicpf);
@@ -288,18 +288,18 @@ int lf_mng_init(struct lf_mng_params *params, struct lf_mng **lf_mng)
 
 void lf_mng_deinit(struct lf_mng *lf_mng)
 {
-	int	 cntr, lf;
+	u8	 cntr, lf;
 
 	if (!lf_mng)
 		return;
 
-
 	for (cntr = 0; cntr < lf_mng->num_containers; cntr++) {
-		for (lf = 0; lf < lf_mng->containers[cntr].num_lfs; lf++)
+		for (lf = 0; lf < lf_mng->containers[cntr].num_lfs; lf++) {
 			if (lf_mng->containers[cntr].lfs[lf].type == NMP_LF_T_NIC_PF)
 				nmnicpf_deinit(lf_mng->containers[cntr].lfs[lf].u.nmnicpf);
 			else
 				pr_warn("LF type %d not supported!\n", lf_mng->containers[cntr].lfs[lf].type);
+		}
 		nmcstm_deinit(lf_mng->containers[cntr].nmcstm);
 	}
 
@@ -309,11 +309,22 @@ void lf_mng_deinit(struct lf_mng *lf_mng)
 int lf_mng_run_maintenance(struct lf_mng *lf_mng)
 {
 	struct nmlf	*nmlf;
-	u8		 cntr = 0; /* TODO: need to iterate all NICPFs! */
-	u8		 lf = 0; /* TODO: need to iterate all NICPFs! */
+	u8		 cntr, lf;
+	int		 ret;
 
-	nmlf = (struct nmlf *)lf_mng->containers[cntr].lfs[lf].u.nmnicpf;
-	return nmlf->f_maintenance_cb(nmlf);
+	if (!lf_mng)
+		return -1;
+
+	for (cntr = 0; cntr < lf_mng->num_containers; cntr++)
+		for (lf = 0; lf < lf_mng->containers[cntr].num_lfs; lf++) {
+			/* As the lf is represented in a union so we can cast one of them and it works on all */
+			nmlf = (struct nmlf *)lf_mng->containers[cntr].lfs[lf].u.nmnicpf;
+			ret = nmlf->f_maintenance_cb(nmlf);
+			if (ret)
+				return ret;
+		}
+
+	return 0;
 }
 
 /* TODO: move this routine to NMP-guest (i.e. should be caled through Qs) */
