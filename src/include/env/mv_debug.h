8b852904c4e464c1edef9d531be2a3b4a5a5ddcd
Meir Levi <mlevi4@marvell.com>
2021-02-03 03:17:25 -0500

    env: add timestamp
    Add timestamp info to MUSDK prints
    
    Change-Id: Ibe93ec2d80ea1d341cff24ddaa898a6def561783
    Signed-off-by: Meir Levi <mlevi4@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/musdk/+/45232
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Liron Himi <lironh@marvell.com>

--- src/include/env/mv_debug.h
+++ src/include/env/mv_debug.h
@@ -99,6 +99,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #ifdef MVCONF_SYSLOG
 #include <syslog.h>
 #endif
@@ -119,10 +120,16 @@
 #define MV_DBG_L_DBG	7
 
 #ifndef MVCONF_SYSLOG
-#define mv_print(_level, fmt, ...)			\
-do {							\
-	if ((_level) <= (MVCONF_DBG_LEVEL))		\
-		printf(log_fmt(fmt, ##__VA_ARGS__));	\
+#define mv_print(_level, fmt, ...)					  \
+do {									  \
+	if ((_level) <= (MVCONF_DBG_LEVEL))	{			  \
+		struct timespec spec;					  \
+		u64 mic_sec;						  \
+		clock_gettime(CLOCK_MONOTONIC, &spec);			  \
+		mic_sec = spec.tv_nsec / 1e3;				  \
+		printf(log_fmt("[%*lu.%lu] " fmt, 5, spec.tv_sec, mic_sec,\
+		##__VA_ARGS__));					  \
+	}								  \
 } while (0)
 #else /* MVCONF_SYSLOG */
 
