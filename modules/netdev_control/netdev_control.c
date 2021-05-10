116f9efb50dbd0dbad6ee22d87046fa1565f5cb2
Alex <alexl@marvell.com>
2021-05-10 08:57:46 -0400

    modules: Introduce netdev_control module
    Utility for enabling/disabling specific network interface,
    from user space
    
    Signed-off-by: Alex <alexl@marvell.com>
    Change-Id: I5fd17ad2e6e7b58f9128ec3b16ab5331402893ec
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/musdk/+/51467
    Reviewed-by: Stefan Chulski <stefanc@marvell.com>
    Reviewed-by: Liron Himi <lironh@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>

---
+++ modules/netdev_control/netdev_control.c
@@ -0,0 +1,80 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (C) 2021 Marvell. */
+
+#include <linux/printk.h>
+#include <linux/sysfs.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/netdevice.h>
+
+#define NETDEV_SYSFS_PORT_INFO_DIR     "/sys/kernel/netdev_control/"
+#define NETDEV_SYSFS_FILE              "netdev_control_state"
+#define MAX_NETDEV_LEN                 256
+
+static ssize_t netdev_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count);
+
+static struct kobj_attribute netdev_mailbox_attribute = __ATTR(net_dev, 0200, NULL, netdev_store);
+struct kobject *netdev_mailbox_kobject;
+
+/* This Function will be called from Init function */
+static int netdev_control_init(void)
+{
+	int err;
+
+	/* Creating a directory in /sys/kernel/ */
+	netdev_mailbox_kobject = kobject_create_and_add(NETDEV_SYSFS_FILE, kernel_kobj);
+	if (!netdev_mailbox_kobject)
+		return -ENOMEM;
+
+	/* Create sysfs file for etx_value /sys/devices/netdev_queue_state */
+	err = sysfs_create_file(netdev_mailbox_kobject, &netdev_mailbox_attribute.attr);
+	if (err)
+		pr_err("failed to create the sysfs file in %s%s\n", NETDEV_SYSFS_PORT_INFO_DIR,
+			NETDEV_SYSFS_FILE);
+	return err;
+}
+
+static ssize_t netdev_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	struct net_device *netdev = NULL;
+	char netdev_name[MAX_NETDEV_LEN];
+	int queue_state, ret;
+
+	memset(netdev_name, 0, MAX_NETDEV_LEN);
+
+	/* Get netdev name */
+	ret = sscanf(buf, "%s %d", &netdev_name[0], &queue_state);
+	if (ret != 2) {
+		pr_err("*** sscanf error: %d", ret);
+		return -EINVAL;
+	}
+
+	/* search by defined interface name */
+	netdev = dev_get_by_name(&init_net, netdev_name);
+	if (!netdev)
+		return -EINVAL;
+
+	if (queue_state == 1)
+		netif_tx_wake_all_queues(netdev);
+	else if (queue_state == 0)
+		netif_tx_stop_all_queues(netdev);
+	else
+		ret = -ENOMEM;
+
+	dev_put(netdev);
+	return ret;
+}
+
+void netdev_control_remove(void)
+{
+	pr_info("***netdev_control_remove\n");
+	kobject_put(netdev_mailbox_kobject);
+	sysfs_remove_file(netdev_mailbox_kobject, &netdev_mailbox_attribute.attr);
+}
+
+module_init(netdev_control_init);
+module_exit(netdev_control_remove);
+MODULE_AUTHOR("MARVELL");
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("Marvell NETDEV_CONTROL");
